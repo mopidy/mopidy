8c7c2a65c9d9793e8ba4167ca629eda155f42c4e
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-12-23 21:02:51 +0100

    Add logger name to log output

--- mopidy/settings.py
+++ mopidy/settings.py
@@ -1,4 +1,4 @@
-CONSOLE_LOG_FORMAT = u'%(levelname)-8s %(asctime)s\n  %(message)s'
+CONSOLE_LOG_FORMAT = u'%(levelname)-8s %(asctime)s %(name)s\n  %(message)s'
 MPD_LINE_ENCODING = u'utf-8'
 MPD_LINE_TERMINATOR = u'\n'
 MPD_SERVER_HOSTNAME = u'localhost'
