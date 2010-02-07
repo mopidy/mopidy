10fa84dd1e49138feb15eee8db83a4b547727b60
Thomas Adamcik <adamcik@samfundet.no>
2010-02-07 05:13:50 +0100

    Add GStreamerCurrentPlaylistHandlerTest

--- tests/backends/gstreamer.py
+++ tests/backends/gstreamer.py
@@ -2,14 +2,18 @@ import unittest
 
 from mopidy.backends.gstreamer import GStreamerBackend
 
-from tests.backends import BasePlaybackControllerTest
+from tests.backends import (BasePlaybackControllerTest,
+                            BaseCurrentPlaylistControllerTest)
 
-class GStreamerPlaybackControllerTest(BasePlaybackControllerTest, unittest.TestCase):
+class GStreamerCurrentPlaylistHandlerTest(BaseCurrentPlaylistControllerTest, unittest.TestCase):
     uri = ['file://data/song1.mp3',
            'file://data/song2.mp3',
            'file://data/song3.mp3',
           ]
 
+    backend_class = GStreamerBackend
+
+class GStreamerPlaybackControllerTest(BasePlaybackControllerTest, unittest.TestCase):
     backend_class = GStreamerBackend
 
 if __name__ == '__main__':
