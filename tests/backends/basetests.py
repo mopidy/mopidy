a5b6e456b97b6db2a10c4a96e1a42563e13ec9ac
Thomas Adamcik <adamcik@samfundet.no>
2010-02-17 22:55:56 +0100

    Add wrapping of __name__ and __doc__

--- tests/backends/basetests.py
+++ tests/backends/basetests.py
@@ -8,6 +8,8 @@ def populate_playlist(func):
             self.backend.current_playlist.add(uri)
         return func(self)
 
+    wrapper.__name__ = func.__name__
+    wrapper.__doc__ = func.__doc__
     return wrapper
 
 class BaseCurrentPlaylistControllerTest(object):
