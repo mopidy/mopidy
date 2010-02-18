b0699b4683a241449889ee712ae57bb13f0e3eaa
Thomas Adamcik <adamcik@samfundet.no>
2010-02-18 02:42:09 +0100

    Use actuall mp3s for testing

--- tests/backends/gstreamer.py
+++ tests/backends/gstreamer.py
@@ -1,23 +1,24 @@
 import unittest
+import os
 
 from mopidy.backends.gstreamer import GStreamerBackend
 
 from tests.backends.basetests import (BasePlaybackControllerTest,
                                       BaseCurrentPlaylistControllerTest)
 
+folder = os.path.dirname(__file__)
+folder = os.path.join(folder, '..', 'data')
+folder = os.path.abspath(folder)
+song = os.path.join(folder, 'song%s.mp3')
+song = 'file://' + song
+
 class GStreamerCurrentPlaylistHandlerTest(BaseCurrentPlaylistControllerTest, unittest.TestCase):
-    uris = ['file://data/song1.mp3',
-            'file://data/song2.mp3',
-            'file://data/song3.mp3',
-           ]
+    uris = [song % i for i in range(1, 4)]
 
     backend_class = GStreamerBackend
 
 class GStreamerPlaybackControllerTest(BasePlaybackControllerTest, unittest.TestCase):
-    uris = ['file://data/song1.mp3',
-            'file://data/song2.mp3',
-            'file://data/song3.mp3',
-           ]
+    uris = [song % i for i in range(1, 4)]
 
     backend_class = GStreamerBackend
     supports_volume = True
