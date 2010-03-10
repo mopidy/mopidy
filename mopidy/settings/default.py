39cfb9a07f271ff7bbc05a3872538d281d3ebd9b
Kristian <klette@samfundet.no>
2010-03-10 22:50:48 +0100

    Fixed doc styling on external mixers part

--- mopidy/settings/default.py
+++ mopidy/settings/default.py
@@ -44,13 +44,15 @@ CONSOLE_LOG_FORMAT = u'%(levelname)-8s %(asctime)s [%(threadName)s] %(name)s\n  
 #: Using external mixers depends on the pyserial-library,
 #: so make sure you have it installed. It also adds one
 #: more setting, MIXER_PORT. This must point to the device
-#: port like /dev/tty1 or similar.
+#: port like /dev/tty1 or similar. Example::
 #:
-#: Available external Mixers::
+#:     MIXER_PORT = u'/dev/ttyUSB0' # Verify this manually
 #:
+#: **Available external Mixers**
+#:
+#: Denon AVR/AVC via RS-232::
 #:
 #:     MIXER = u'mopidy.mixers.denon.DenonMixer'
-#:     MIXER_PORT = u'/dev/tty0' # Verify this manually
 #:
 MIXER = u'mopidy.mixers.dummy.DummyMixer'
 if sys.platform == 'linux2':
@@ -58,7 +60,7 @@ if sys.platform == 'linux2':
 elif sys.platform == 'darwin':
     MIXER = u'mopidy.mixers.osa.OsaMixer'
 
-MIXER_PORT=None
+MIXER_PORT = None
 
 #: Which address Mopidy should bind to. Examples:
 #:
