9437db3c38cdace7fb61eca2d95d479632b396ce
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-03-13 00:35:37 +0100

    DenonMixer: Add readline() method to mock

--- tests/mixers/denontest.py
+++ tests/mixers/denontest.py
@@ -11,10 +11,16 @@ class DenonMixerDeviceMock(object):
     def write(self, x):
         if x[2] != '?':
             self.ret_val = bytes(x)
+
     def read(self, x):
         return self.ret_val
+
+    def readline(self):
+        return self.ret_val
+
     def isOpen(self):
         return self._open
+
     def open(self):
         self._open = True
 
