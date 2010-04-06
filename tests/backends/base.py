be952b01731891ff773b1e6b0da03a885f6498bd
Thomas Adamcik <adamcik@samfundet.no>
2010-04-06 23:02:48 +0200

    Rename test_play_track_with_random_removes_track to test_played_track_during_random_not_played_again

--- tests/backends/base.py
+++ tests/backends/base.py
@@ -780,11 +780,12 @@ class BasePlaybackControllerTest(object):
         self.assertNotEqual(self.playback.next_track, None)
 
     @populate_playlist
-    def test_play_track_with_random_removes_track(self):
+    def test_played_track_during_random_not_played_again(self):
         self.playback.random = True
         played = []
         for track in self.tracks:
             self.playback.next()
-            self.assert_(self.playback.current_track not in played)
+            self.assert_(self.playback.current_track not in played,
+                self.playback.current_track)
             played.append(self.playback.current_track)
         self.assertEqual(self.playback.next_track, None)
