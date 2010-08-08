a6d06a6c4bab25cd552a60f6bb8a7fbebf816972
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-08-08 18:38:29 +0200

    Remove Track.id usage from de-/libspotify integration tests

--- tests/backends/libspotify_integrationtest.py
+++ tests/backends/libspotify_integrationtest.py
@@ -15,13 +15,13 @@ uris = [
 
 class LibspotifyCurrentPlaylistControllerTest(
         BaseCurrentPlaylistControllerTest, unittest.TestCase):
-    tracks = [Track(uri=uri, id=i, length=4464) for i, uri in enumerate(uris)]
+    tracks = [Track(uri=uri, length=4464) for i, uri in enumerate(uris)]
     backend_class = LibspotifyBackend
 
 
 class LibspotifyPlaybackControllerTest(
         BasePlaybackControllerTest, unittest.TestCase):
-    tracks = [Track(uri=uri, id=i, length=4464) for i, uri in enumerate(uris)]
+    tracks = [Track(uri=uri, length=4464) for i, uri in enumerate(uris)]
     backend_class = LibspotifyBackend
 
 
