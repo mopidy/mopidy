63d2e7710e6088cee4162c9987c4ba4179d32965
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-08-14 00:50:22 +0200

    Copy distutils install_data fix from Django

--- setup.py
+++ setup.py
@@ -1,8 +1,33 @@
+"""
+Most of this file is taken from the Django project, which is BSD licensed.
+"""
+
 from distutils.core import setup
+from distutils.command.install_data import install_data
 from distutils.command.install import INSTALL_SCHEMES
 import os
+import sys
 
 from mopidy import get_version
+
+class osx_install_data(install_data):
+    # On MacOS, the platform-specific lib dir is
+    # /System/Library/Framework/Python/.../ which is wrong. Python 2.5 supplied
+    # with MacOS 10.5 has an Apple-specific fix for this in
+    # distutils.command.install_data#306. It fixes install_lib but not
+    # install_data, which is why we roll our own install_data class.
+
+    def finalize_options(self):
+        # By the time finalize_options is called, install.install_lib is set to
+        # the fixed directory, so we set the installdir to install_lib. The
+        # install_data class uses ('install_data', 'install_dir') instead.
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+
+if sys.platform == "darwin":
+    cmdclasses = {'install_data': osx_install_data}
+else:
+    cmdclasses = {'install_data': install_data}
 
 def fullsplit(path, result=None):
     """
@@ -20,7 +45,8 @@ def fullsplit(path, result=None):
 
 # Tell distutils to put the data_files in platform-specific installation
 # locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+# http://groups.google.com/group/comp.lang.python/browse_thread/
+# thread/35ec7b2fed36eaec/2105ee4d9e8042cb
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
 
@@ -49,6 +75,7 @@ setup(
     author='Stein Magnus Jodal',
     author_email='stein.magnus@jodal.no',
     packages=packages,
+    cmdclass=cmdclasses,
     data_files=data_files,
     scripts=['bin/mopidy'],
     url='http://www.mopidy.com/',
