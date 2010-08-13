581d694cb1a660a0ce6dd4399ec37394858e0edd
Thomas Adamcik <adamcik@samfundet.no>
2010-08-13 21:51:02 +0200

    Split libspotify backend into one file per class, and thus ensure that spotify depenedices don't fail tests

---
+++ mopidy/backends/libspotify/playback.py
@@ -0,0 +1,51 @@
+import logging
+import multiprocessing
+
+from spotify import Link, SpotifyError
+
+from mopidy.backends.base import BasePlaybackController
+from mopidy.process import pickle_connection
+
+logger = logging.getLogger('mopidy.backends.libspotify.playback')
+
+class LibspotifyPlaybackController(BasePlaybackController):
+    def _set_output_state(self, state_name):
+        logger.debug(u'Setting output state to %s ...', state_name)
+        (my_end, other_end) = multiprocessing.Pipe()
+        self.backend.output_queue.put({
+            'command': 'set_state',
+            'state': state_name,
+            'reply_to': pickle_connection(other_end),
+        })
+        my_end.poll(None)
+        return my_end.recv()
+
+    def _pause(self):
+        return self._set_output_state('PAUSED')
+
+    def _play(self, track):
+        self._set_output_state('READY')
+        if self.state == self.PLAYING:
+            self.stop()
+        if track.uri is None:
+            return False
+        try:
+            self.backend.spotify.session.load(
+                Link.from_string(track.uri).as_track())
+            self.backend.spotify.session.play(1)
+            self._set_output_state('PLAYING')
+            return True
+        except SpotifyError as e:
+            logger.warning('Play %s failed: %s', track.uri, e)
+            return False
+
+    def _resume(self):
+        return self._set_output_state('PLAYING')
+
+    def _seek(self, time_position):
+        pass # TODO
+
+    def _stop(self):
+        result = self._set_output_state('READY')
+        self.backend.spotify.session.play(0)
+        return result
---
+++ mopidy/backends/libspotify/session_manager.py
@@ -0,0 +1,106 @@
+import logging
+import os
+import threading
+
+from spotify.manager import SpotifySessionManager
+
+from mopidy import get_version, settings
+from mopidy.models import Playlist
+from mopidy.backends.libspotify.translator import LibspotifyTranslator
+
+logger = logging.getLogger('mopidy.backends.libspotify.session_manager')
+
+class LibspotifySessionManager(SpotifySessionManager, threading.Thread):
+    cache_location = os.path.expanduser(settings.SPOTIFY_LIB_CACHE)
+    settings_location = os.path.expanduser(settings.SPOTIFY_LIB_CACHE)
+    appkey_file = os.path.expanduser(settings.SPOTIFY_LIB_APPKEY)
+    user_agent = 'Mopidy %s' % get_version()
+
+    def __init__(self, username, password, core_queue, output_queue):
+        SpotifySessionManager.__init__(self, username, password)
+        threading.Thread.__init__(self)
+        self.core_queue = core_queue
+        self.output_queue = output_queue
+        self.connected = threading.Event()
+        self.session = None
+
+    def run(self):
+        self.connect()
+
+    def logged_in(self, session, error):
+        """Callback used by pyspotify"""
+        logger.info('Logged in')
+        self.session = session
+        self.connected.set()
+
+    def logged_out(self, session):
+        """Callback used by pyspotify"""
+        logger.info('Logged out')
+
+    def metadata_updated(self, session):
+        """Callback used by pyspotify"""
+        logger.debug('Metadata updated, refreshing stored playlists')
+        playlists = []
+        for spotify_playlist in session.playlist_container():
+            playlists.append(
+                LibspotifyTranslator.to_mopidy_playlist(spotify_playlist))
+        self.core_queue.put({
+            'command': 'set_stored_playlists',
+            'playlists': playlists,
+        })
+
+    def connection_error(self, session, error):
+        """Callback used by pyspotify"""
+        logger.error('Connection error: %s', error)
+
+    def message_to_user(self, session, message):
+        """Callback used by pyspotify"""
+        logger.info(message)
+
+    def notify_main_thread(self, session):
+        """Callback used by pyspotify"""
+        logger.debug('Notify main thread')
+
+    def music_delivery(self, session, frames, frame_size, num_frames,
+            sample_type, sample_rate, channels):
+        """Callback used by pyspotify"""
+        # TODO Base caps_string on arguments
+        caps_string = """
+            audio/x-raw-int,
+            endianness=(int)1234,
+            channels=(int)2,
+            width=(int)16,
+            depth=(int)16,
+            signed=True,
+            rate=(int)44100
+        """
+        self.output_queue.put({
+            'command': 'deliver_data',
+            'caps': caps_string,
+            'data': bytes(frames),
+        })
+
+    def play_token_lost(self, session):
+        """Callback used by pyspotify"""
+        logger.debug('Play token lost')
+        self.core_queue.put({'command': 'stop_playback'})
+
+    def log_message(self, session, data):
+        """Callback used by pyspotify"""
+        logger.debug(data)
+
+    def end_of_track(self, session):
+        """Callback used by pyspotify"""
+        logger.debug('End of data stream.')
+        self.output_queue.put({'command': 'end_of_data_stream'})
+
+    def search(self, query, connection):
+        """Search method used by Mopidy backend"""
+        def callback(results, userdata):
+            # TODO Include results from results.albums(), etc. too
+            playlist = Playlist(tracks=[
+                LibspotifyTranslator.to_mopidy_track(t)
+                for t in results.tracks()])
+            connection.send(playlist)
+        self.connected.wait()
+        self.session.search(query, callback)
