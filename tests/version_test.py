9e7723f8cabcfe28a8d1a2178f1deb321c6580cc
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-08-22 22:08:40 +0200

    Update version tests for 0.1.0

--- tests/version_test.py
+++ tests/version_test.py
@@ -9,10 +9,9 @@ class VersionTest(unittest.TestCase):
 
     def test_versions_can_be_strictly_ordered(self):
         self.assert_(SV('0.1.0a0') < SV('0.1.0a1'))
-        self.assert_(SV('0.1.0a2') < SV(get_version()))
+        self.assert_(SV('0.1.0a1') < SV('0.1.0a2'))
+        self.assert_(SV('0.1.0a2') < SV('0.1.0a3'))
         self.assert_(SV('0.1.0a3') < SV(get_version()))
-        self.assert_(SV(get_version()) < SV('0.1.0a5'))
-        self.assert_(SV('0.1.0a0') < SV('0.1.0'))
-        self.assert_(SV('0.1.0') < SV('0.1.1'))
+        self.assert_(SV(get_version()) < SV('0.1.1'))
         self.assert_(SV('0.1.1') < SV('0.2.0'))
         self.assert_(SV('0.2.0') < SV('1.0.0'))
