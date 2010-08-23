5d61bb1f7d2e6233c72d34a454d406a4e9d94ade
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-08-23 23:40:48 +0200

    BaseOutput takes a reference to core_queue

--- mopidy/outputs/base.py
+++ mopidy/outputs/base.py
@@ -3,6 +3,9 @@ class BaseOutput(object):
     Base class for audio outputs.
     """
 
+    def __init__(self, core_queue):
+        self.core_queue = core_queue
+
     def start(self):
         """Start the output."""
         pass
