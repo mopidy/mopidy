8b963311e1e1449fd34c1a68bbe859fe51cb9fdb
Thomas Adamcik <adamcik@samfundet.no>
2010-10-26 21:38:46 +0200

    Update scanner test

--- tests/scanner.py
+++ tests/scanner.py
@@ -14,32 +14,34 @@ class ScannerTest(unittest.TestCase):
             self.data_callback, self.error_callback)
         scanner.start()
 
+    def check(self, name, key, value):
+        name = data_folder(name)
+        self.assertEqual(self.data[name][key], value)
+
     def data_callback(self, uri, data):
-        uri = uri.lstrip('file://')
-        uri = uri.lstrip(data_folder(''))
+        uri = uri[len('file://'):]
         self.data[uri] = data
 
     def error_callback(self, uri, errors):
-        uri = uri.lstrip('file://')
-        uri = uri.lstrip(data_folder(''))
+        uri = uri[len('file://'):]
         self.errors[uri] = errors
 
     def test_data_is_set(self):
-        self.scan('blank.mp3')
+        self.scan('scanner/simple')
         self.assert_(self.data)
 
     def test_errors_is_not_set(self):
-        self.scan('blank.mp3')
+        self.scan('scanner/simple')
         self.assert_(not self.errors)
 
     def test_artist_is_set(self):
-        self.scan('blank.mp3')
-        self.assertEqual(self.data['blank.mp3']['artist'], 'artist')
+        self.scan('scanner/simple')
+        self.check('scanner/simple/song1.mp3', 'artist', 'name')
 
     def test_album_is_set(self):
-        self.scan('blank.mp3')
-        self.assertEqual(self.data['blank.mp3']['album'], 'album')
+        self.scan('scanner/simple')
+        self.check('scanner/simple/song1.mp3', 'album', 'albumname')
 
     def test_track_is_set(self):
-        self.scan('blank.mp3')
-        self.assertEqual(self.data['blank.mp3']['title'], 'title')
+        self.scan('scanner/simple')
+        self.check('scanner/simple/song1.mp3', 'title', 'trackname')
