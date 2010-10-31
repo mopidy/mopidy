71ee3a8ef74db145af731a0b2fd46b9c029e7bc9
Thomas Adamcik <adamcik@samfundet.no>
2010-11-01 00:07:59 +0100

    Don't remove expanduser from get_or_create_file and get_or_create_folder

--- mopidy/utils/path.py
+++ mopidy/utils/path.py
@@ -7,12 +7,14 @@ import urllib
 logger = logging.getLogger('mopidy.utils.path')
 
 def get_or_create_folder(folder):
+    folder = os.path.expanduser(folder)
     if not os.path.isdir(folder):
         logger.info(u'Creating dir %s', folder)
         os.mkdir(folder, 0755)
     return folder
 
 def get_or_create_file(filename):
+    filename = os.path.expanduser(filename)
     if not os.path.isfile(filename):
         logger.info(u'Creating file %s', filename)
         open(filename, 'w')
