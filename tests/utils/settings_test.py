e8371129f74a696b004ac3bd7ad6ee893d7e455d
Thomas Adamcik <adamcik@samfundet.no>
2010-10-31 23:19:30 +0100

    Add test to ensure that other values are not expanded

--- tests/utils/settings_test.py
+++ tests/utils/settings_test.py
@@ -73,8 +73,12 @@ class SettingsProxyTest(unittest.TestCase):
         expected = os.path.expanduser('~/test')
         self.assertEqual(acctual, expected)
 
-    def test_value_ending_in_path_is_absolute(self):
-        self.settings.TEST_PATH = './test'
-        acctual = self.settings.TEST_PATH
-        expected = os.path.abspath('./test')
-        self.assertEqual(acctual, expected)
+    def test_value_not_ending_in_path_is_not_expanded(self):
+        self.settings.TEST = '~/test'
+        acctual = self.settings.TEST
+        self.assertEqual(acctual, '~/test')
+
+    def test_value_not_ending_in_path_is_not_absolute(self):
+        self.settings.TEST = './test'
+        acctual = self.settings.TEST
+        self.assertEqual(acctual, './test')
