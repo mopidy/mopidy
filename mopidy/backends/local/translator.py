91f9180c39b19f329bf23c9fbac44915b878b933
Thomas Adamcik <adamcik@samfundet.no>
2010-11-01 00:33:32 +0100

    Add some basic fixmes

--- mopidy/backends/local/translator.py
+++ mopidy/backends/local/translator.py
@@ -106,6 +106,7 @@ def _convert_mpd_data(data, tracks, music_dir):
         track_kwargs['artists'] = [artist]
         album_kwargs['artists'] = [artist]
 
+    # FIXME Newer mpd tag caches support albumartist names
     if 'album' in data:
         album_kwargs['name'] = data['album']
         album = Album(**album_kwargs)
@@ -114,10 +115,14 @@ def _convert_mpd_data(data, tracks, music_dir):
     if 'title' in data:
         track_kwargs['name'] = data['title']
 
+    # FIXME what if file is uri - generated tag cache needs to allways make
+    # LOCAL_MUSIC_PATH relative paths or this code must handle uris
     if data['file'][0] == '/':
         path = data['file'][1:]
     else:
         path = data['file']
+
+    # FIXME newer mpd tag caches provide musicbrainz ids
 
     track_kwargs['uri'] = path_to_uri(music_dir, path)
     track_kwargs['length'] = int(data.get('time', 0)) * 1000
