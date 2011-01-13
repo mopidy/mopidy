79bd041924ba5a5233870ee3f62d4514ea375fe6
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-01-13 15:57:49 +0100

    docs: Add 'this page' source link to make it easier to understand how the page is made and thus contribute fixes

--- docs/conf.py
+++ docs/conf.py
@@ -153,7 +153,7 @@ html_last_updated_fmt = '%b %d, %Y'
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-html_show_sourcelink = False
+#html_show_sourcelink = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
