55b32065a8dd32c5d3fa6660916c5f63fbd1a201
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-01-21 16:38:16 +0100

    Expose MPD error codes as constants on the MpdAckError exception, to avoid use of magic values in the other exceptions

--- mopidy/frontends/mpd/exceptions.py
+++ mopidy/frontends/mpd/exceptions.py
@@ -1,22 +1,20 @@
 from mopidy import MopidyException
 
 class MpdAckError(MopidyException):
-    """
-    Available MPD error codes::
+    """See fields on this class for available MPD error codes"""
 
-        ACK_ERROR_NOT_LIST = 1
-        ACK_ERROR_ARG = 2
-        ACK_ERROR_PASSWORD = 3
-        ACK_ERROR_PERMISSION = 4
-        ACK_ERROR_UNKNOWN = 5
-        ACK_ERROR_NO_EXIST = 50
-        ACK_ERROR_PLAYLIST_MAX = 51
-        ACK_ERROR_SYSTEM = 52
-        ACK_ERROR_PLAYLIST_LOAD = 53
-        ACK_ERROR_UPDATE_ALREADY = 54
-        ACK_ERROR_PLAYER_SYNC = 55
-        ACK_ERROR_EXIST = 56
-    """
+    ACK_ERROR_NOT_LIST = 1
+    ACK_ERROR_ARG = 2
+    ACK_ERROR_PASSWORD = 3
+    ACK_ERROR_PERMISSION = 4
+    ACK_ERROR_UNKNOWN = 5
+    ACK_ERROR_NO_EXIST = 50
+    ACK_ERROR_PLAYLIST_MAX = 51
+    ACK_ERROR_SYSTEM = 52
+    ACK_ERROR_PLAYLIST_LOAD = 53
+    ACK_ERROR_UPDATE_ALREADY = 54
+    ACK_ERROR_PLAYER_SYNC = 55
+    ACK_ERROR_EXIST = 56
 
     def __init__(self, message=u'', error_code=0, index=0, command=u''):
         super(MpdAckError, self).__init__(message, error_code, index, command)
@@ -37,24 +35,24 @@ class MpdAckError(MopidyException):
 class MpdArgError(MpdAckError):
     def __init__(self, *args, **kwargs):
         super(MpdArgError, self).__init__(*args, **kwargs)
-        self.error_code = 2 # ACK_ERROR_ARG
+        self.error_code = MpdAckError.ACK_ERROR_ARG
 
 class MpdPasswordError(MpdAckError):
     def __init__(self, *args, **kwargs):
         super(MpdPasswordError, self).__init__(*args, **kwargs)
-        self.error_code = 3 # ACK_ERROR_PASSWORD
+        self.error_code = MpdAckError.ACK_ERROR_PASSWORD
 
 class MpdUnknownCommand(MpdAckError):
     def __init__(self, *args, **kwargs):
         super(MpdUnknownCommand, self).__init__(*args, **kwargs)
         self.message = u'unknown command "%s"' % self.command
         self.command = u''
-        self.error_code = 5 # ACK_ERROR_UNKNOWN
+        self.error_code = MpdAckError.ACK_ERROR_UNKNOWN
 
 class MpdNoExistError(MpdAckError):
     def __init__(self, *args, **kwargs):
         super(MpdNoExistError, self).__init__(*args, **kwargs)
-        self.error_code = 50 # ACK_ERROR_NO_EXIST
+        self.error_code = MpdAckError.ACK_ERROR_NO_EXIST
 
 class MpdNotImplemented(MpdAckError):
     def __init__(self, *args, **kwargs):
