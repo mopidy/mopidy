89104bd326587b84b8af5dd6151e836baa20ae22
Thomas Adamcik <adamcik@samfundet.no>
2011-06-04 01:52:11 +0200

    Add skipped test case for date missing in scanner tests

--- tests/scanner_test.py
+++ tests/scanner_test.py
@@ -4,7 +4,7 @@ from datetime import date
 from mopidy.scanner import Scanner, translator
 from mopidy.models import Track, Artist, Album
 
-from tests import path_to_data_dir
+from tests import path_to_data_dir, SkipTest
 
 class FakeGstDate(object):
     def __init__(self, year, month, day):
@@ -184,3 +184,7 @@ class ScannerTest(unittest.TestCase):
     def test_other_media_is_ignored(self):
         self.scan('scanner/image')
         self.assert_(self.errors)
+
+    @SkipTest
+    def test_song_without_time_is_handeled(self):
+        pass
