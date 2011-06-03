cbdc37a24da152c9d3d007a9fccd89e8131f4b04
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-06-03 16:52:52 +0200

    Replace all single letter instance variables in MPD protocol tests

--- tests/frontends/mpd/connection_test.py
+++ tests/frontends/mpd/connection_test.py
@@ -2,47 +2,47 @@ import unittest
 
 from mopidy import settings
 from mopidy.backends.dummy import DummyBackend
-from mopidy.frontends.mpd import dispatcher
+from mopidy.frontends.mpd.dispatcher import MpdDispatcher
 from mopidy.mixers.dummy import DummyMixer
 
 class ConnectionHandlerTest(unittest.TestCase):
     def setUp(self):
-        self.b = DummyBackend.start().proxy()
+        self.backend = DummyBackend.start().proxy()
         self.mixer = DummyMixer.start().proxy()
-        self.h = dispatcher.MpdDispatcher()
+        self.dispatcher = MpdDispatcher()
 
     def tearDown(self):
-        self.b.stop().get()
+        self.backend.stop().get()
         self.mixer.stop().get()
         settings.runtime.clear()
 
     def test_close(self):
-        result = self.h.handle_request(u'close')
+        result = self.dispatcher.handle_request(u'close')
         self.assert_(u'OK' in result)
 
     def test_empty_request(self):
-        result = self.h.handle_request(u'')
+        result = self.dispatcher.handle_request(u'')
         self.assert_(u'OK' in result)
 
     def test_kill(self):
-        result = self.h.handle_request(u'kill')
+        result = self.dispatcher.handle_request(u'kill')
         self.assert_(u'OK' in result)
 
     def test_valid_password_is_accepted(self):
         settings.MPD_SERVER_PASSWORD = u'topsecret'
-        result = self.h.handle_request(u'password "topsecret"')
+        result = self.dispatcher.handle_request(u'password "topsecret"')
         self.assert_(u'OK' in result)
 
     def test_invalid_password_is_not_accepted(self):
         settings.MPD_SERVER_PASSWORD = u'topsecret'
-        result = self.h.handle_request(u'password "secret"')
+        result = self.dispatcher.handle_request(u'password "secret"')
         self.assert_(u'ACK [3@0] {password} incorrect password' in result)
 
     def test_any_password_is_not_accepted_when_password_check_turned_off(self):
         settings.MPD_SERVER_PASSWORD = None
-        result = self.h.handle_request(u'password "secret"')
+        result = self.dispatcher.handle_request(u'password "secret"')
         self.assert_(u'ACK [3@0] {password} incorrect password' in result)
 
     def test_ping(self):
-        result = self.h.handle_request(u'ping')
+        result = self.dispatcher.handle_request(u'ping')
         self.assert_(u'OK' in result)
--- tests/frontends/mpd/reflection_test.py
+++ tests/frontends/mpd/reflection_test.py
@@ -1,21 +1,21 @@
 import unittest
 
 from mopidy.backends.dummy import DummyBackend
-from mopidy.frontends.mpd import dispatcher
+from mopidy.frontends.mpd.dispatcher import MpdDispatcher
 from mopidy.mixers.dummy import DummyMixer
 
 class ReflectionHandlerTest(unittest.TestCase):
     def setUp(self):
-        self.b = DummyBackend.start().proxy()
+        self.backend = DummyBackend.start().proxy()
         self.mixer = DummyMixer.start().proxy()
-        self.h = dispatcher.MpdDispatcher()
+        self.dispatcher = MpdDispatcher()
 
     def tearDown(self):
-        self.b.stop().get()
+        self.backend.stop().get()
         self.mixer.stop().get()
 
     def test_commands_returns_list_of_all_commands(self):
-        result = self.h.handle_request(u'commands')
+        result = self.dispatcher.handle_request(u'commands')
         # Check if some random commands are included
         self.assert_(u'command: commands' in result)
         self.assert_(u'command: play' in result)
@@ -30,19 +30,19 @@ class ReflectionHandlerTest(unittest.TestCase):
         self.assert_(u'OK' in result)
 
     def test_decoders(self):
-        result = self.h.handle_request(u'decoders')
+        result = self.dispatcher.handle_request(u'decoders')
         self.assert_(u'ACK [0@0] {} Not implemented' in result)
 
     def test_notcommands_returns_only_ok(self):
-        result = self.h.handle_request(u'notcommands')
+        result = self.dispatcher.handle_request(u'notcommands')
         self.assertEqual(1, len(result))
         self.assert_(u'OK' in result)
 
     def test_tagtypes(self):
-        result = self.h.handle_request(u'tagtypes')
+        result = self.dispatcher.handle_request(u'tagtypes')
         self.assert_(u'OK' in result)
 
     def test_urlhandlers(self):
-        result = self.h.handle_request(u'urlhandlers')
+        result = self.dispatcher.handle_request(u'urlhandlers')
         self.assert_(u'OK' in result)
         self.assert_(u'handler: dummy:' in result)
