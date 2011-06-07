d664c11e2285560e56aaaec646a578fa64b84d7e
Thomas Adamcik <adamcik@samfundet.no>
2011-06-07 14:09:15 +0200

    Pull network related functions out of mopidy.frontends.mpd.server

--- mopidy/frontends/mpd/server.py
+++ mopidy/frontends/mpd/server.py
@@ -1,27 +1,12 @@
 import asyncore
 import logging
-import re
-import socket
 import sys
 
 from mopidy import settings
+from mopidy.utils import network
 from .session import MpdSession
 
 logger = logging.getLogger('mopidy.frontends.mpd.server')
-
-def _try_ipv6_socket():
-    """Determine if system really supports IPv6"""
-    if not socket.has_ipv6:
-        return False
-    try:
-        socket.socket(socket.AF_INET6).close()
-        return True
-    except IOError, e:
-        logger.debug(u'Platform supports IPv6, but socket '
-            'creation failed, disabling: %s', e)
-    return False
-
-has_ipv6 = _try_ipv6_socket()
 
 class MpdServer(asyncore.dispatcher):
     """
@@ -35,15 +20,9 @@ class MpdServer(asyncore.dispatcher):
     def start(self):
         """Start MPD server."""
         try:
-            if has_ipv6:
-                self.create_socket(socket.AF_INET6, socket.SOCK_STREAM)
-                # Explicitly configure socket to work for both IPv4 and IPv6
-                self.socket.setsockopt(
-                    socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
-            else:
-                self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket = network.create_socket()
             self.set_reuse_addr()
-            hostname = self._format_hostname(settings.MPD_SERVER_HOSTNAME)
+            hostname = network.format_hostname(settings.MPD_SERVER_HOSTNAME)
             port = settings.MPD_SERVER_PORT
             logger.debug(u'MPD server is binding to [%s]:%s', hostname, port)
             self.bind((hostname, port))
@@ -65,9 +44,3 @@ class MpdServer(asyncore.dispatcher):
     def handle_close(self):
         """Handle end of client connection."""
         self.close()
-
-    def _format_hostname(self, hostname):
-        if (has_ipv6
-            and re.match('\d+.\d+.\d+.\d+', hostname) is not None):
-            hostname = '::ffff:%s' % hostname
-        return hostname
