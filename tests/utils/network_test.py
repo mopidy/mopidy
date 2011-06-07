14a9a3fb6684f3eab628d59adacbff0a154716e7
Thomas Adamcik <adamcik@samfundet.no>
2011-06-07 15:25:48 +0200

    Use mocking for network.has_ipv6 monkey patching

--- tests/utils/network_test.py
+++ tests/utils/network_test.py
@@ -4,17 +4,13 @@ import unittest
 from mopidy.utils import network
 
 class FormatHostnameTest(unittest.TestCase):
-    def setUp(self):
-        self.has_ipv6 = network.has_ipv6
-
-    def tearDown(self):
-        network.has_ipv6 = self.has_ipv6
-
+    @mock.patch('mopidy.utils.network.has_ipv6', True)
     def test_format_hostname_prefixes_ipv4_addresses_when_ipv6_available(self):
         network.has_ipv6 = True
         self.assertEqual(network.format_hostname('0.0.0.0'), '::ffff:0.0.0.0')
         self.assertEqual(network.format_hostname('1.0.0.1'), '::ffff:1.0.0.1')
 
+    @mock.patch('mopidy.utils.network.has_ipv6', False)
     def test_format_hostname_does_nothing_when_only_ipv4_available(self):
         network.has_ipv6 = False
         self.assertEquals(network.format_hostname('0.0.0.0'), '0.0.0.0')
