24dbba2fa9d3f2dc76657a2d631e8f6f15232a7f
Thomas Adamcik <adamcik@samfundet.no>
2011-06-16 23:17:37 +0200

    Switch to async globject based loop

--- mopidy/frontends/mpd/server.py
+++ mopidy/frontends/mpd/server.py
@@ -1,14 +1,17 @@
-import asyncore
 import logging
 import sys
 
+import gobject
+
 from mopidy import settings
 from mopidy.utils import network
-from .session import MpdSession
+from mopidy.frontends.mpd.dispatcher import MpdDispatcher
+from mopidy.frontends.mpd.protocol import ENCODING, LINE_TERMINATOR, VERSION
+from mopidy.utils.log import indent
 
 logger = logging.getLogger('mopidy.frontends.mpd.server')
 
-class MpdServer(asyncore.dispatcher):
+class MpdServer(object):
     """
     The MPD server. Creates a :class:`mopidy.frontends.mpd.session.MpdSession`
     for each client connection.
@@ -17,22 +20,46 @@ class MpdServer(asyncore.dispatcher):
     def start(self):
         """Start MPD server."""
         try:
-            self.set_socket(network.create_socket())
-            self.set_reuse_addr()
             hostname = network.format_hostname(settings.MPD_SERVER_HOSTNAME)
             port = settings.MPD_SERVER_PORT
             logger.debug(u'MPD server is binding to [%s]:%s', hostname, port)
-            self.bind((hostname, port))
-            self.listen(1)
+            network.Listener((hostname, port), handler=MpdHandler)
             logger.info(u'MPD server running at [%s]:%s', hostname, port)
         except IOError, e:
             logger.error(u'MPD server startup failed: %s' %
                 str(e).decode('utf-8'))
             sys.exit(1)
 
-    def handle_accept(self):
-        """Called by asyncore when a new client connects."""
-        (client_socket, client_socket_address) = self.accept()
-        logger.info(u'MPD client connection from [%s]:%s',
-            client_socket_address[0], client_socket_address[1])
-        MpdSession(self, client_socket, client_socket_address)
+
+class MpdHandler(network.BaseHandler):
+    """
+    The MPD client session. Keeps track of a single client session. Any
+    requests from the client is passed on to the MPD request dispatcher.
+    """
+
+    terminator = LINE_TERMINATOR
+
+    def __init__(self, (sock, addr)):
+        super(MpdHandler, self).__init__((sock, addr))
+        self.dispatcher = MpdDispatcher(session=self)
+        self.send_response([u'OK MPD %s' % VERSION])
+
+    def recv(self, line):
+        """Handle the request using the MPD command handlers."""
+        request = line.decode(ENCODING)
+        logger.debug(u'Request from [%s]:%s: %s', self.addr[0],
+            self.addr[1], indent(request))
+        self.send_response(self.dispatcher.handle_request(request))
+
+    def send_response(self, response):
+        """
+        Format a response from the MPD command handlers and send it to the
+        client.
+        """
+        if response:
+            response = LINE_TERMINATOR.join(response)
+            logger.debug(u'Response to [%s]:%s: %s', self.addr[0],
+                self.addr[1], indent(response))
+            response = u'%s%s' % (response, LINE_TERMINATOR)
+            data = response.encode(ENCODING)
+            self.send(data)
