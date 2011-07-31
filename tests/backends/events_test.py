662a17e7ba1d23423f6301fc2a8ce759d2a71f78
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-07-31 17:05:18 +0200

    Use mock.patch instead of assigning a mock to the imported module

--- tests/backends/events_test.py
+++ tests/backends/events_test.py
@@ -3,54 +3,49 @@ import unittest
 
 from pykka.registry import ActorRegistry
 
-from mopidy import listeners
 from mopidy.backends.dummy import DummyBackend
+from mopidy.listeners import BackendListener
 from mopidy.models import Track
 
+@mock.patch.object(BackendListener, 'send')
 class BackendEventsTest(unittest.TestCase):
     def setUp(self):
-        self.listener_send = mock.Mock()
-        listeners.BackendListener.send = self.listener_send
         self.backend = DummyBackend.start().proxy()
 
     def tearDown(self):
         ActorRegistry.stop_all()
 
-    def test_pause_sends_track_playback_paused_event(self):
+    def test_pause_sends_track_playback_paused_event(self, send):
         self.backend.current_playlist.add(Track(uri='a'))
         self.backend.playback.play().get()
-        self.listener_send.reset_mock()
+        send.reset_mock()
         self.backend.playback.pause().get()
-        self.assertEqual(self.listener_send.call_args[0][0],
-            'track_playback_paused')
+        self.assertEqual(send.call_args[0][0], 'track_playback_paused')
 
-    def test_resume_sends_track_playback_resumed(self):
+    def test_resume_sends_track_playback_resumed(self, send):
         self.backend.current_playlist.add(Track(uri='a'))
         self.backend.playback.play()
         self.backend.playback.pause().get()
-        self.listener_send.reset_mock()
+        send.reset_mock()
         self.backend.playback.resume().get()
-        self.assertEqual(self.listener_send.call_args[0][0],
-            'track_playback_resumed')
+        self.assertEqual(send.call_args[0][0], 'track_playback_resumed')
 
-    def test_play_sends_track_playback_started_event(self):
+    def test_play_sends_track_playback_started_event(self, send):
         self.backend.current_playlist.add(Track(uri='a'))
-        self.listener_send.reset_mock()
+        send.reset_mock()
         self.backend.playback.play().get()
-        self.assertEqual(self.listener_send.call_args[0][0],
-            'track_playback_started')
+        self.assertEqual(send.call_args[0][0], 'track_playback_started')
 
-    def test_stop_sends_track_playback_ended_event(self):
+    def test_stop_sends_track_playback_ended_event(self, send):
         self.backend.current_playlist.add(Track(uri='a'))
         self.backend.playback.play().get()
-        self.listener_send.reset_mock()
+        send.reset_mock()
         self.backend.playback.stop().get()
-        self.assertEqual(self.listener_send.call_args_list[0][0][0],
-            'track_playback_ended')
+        self.assertEqual(send.call_args_list[0][0][0], 'track_playback_ended')
 
-    def test_seek_sends_seeked_event(self):
+    def test_seek_sends_seeked_event(self, send):
         self.backend.current_playlist.add(Track(uri='a', length=40000))
         self.backend.playback.play().get()
-        self.listener_send.reset_mock()
+        send.reset_mock()
         self.backend.playback.seek(1000).get()
-        self.assertEqual(self.listener_send.call_args[0][0], 'seeked')
+        self.assertEqual(send.call_args[0][0], 'seeked')
