5f23b5eafee4be9fb63a14b7a17e11a38091a42c
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-09-30 23:39:14 +0200

    Check Pykka version on startup

--- mopidy/__init__.py
+++ mopidy/__init__.py
@@ -1,6 +1,19 @@
+# pylint: disable = E0611,F0401
+from distutils.version import StrictVersion as SV
+# pylint: enable = E0611,F0401
 import sys
+
+import pykka
+
 if not (2, 6) <= sys.version_info < (3,):
-    sys.exit(u'Mopidy requires Python >= 2.6, < 3')
+    sys.exit(
+        u'Mopidy requires Python >= 2.6, < 3, but found %s' %
+        '.'.join(map(str, sys.version_info[:3])))
+
+if (isinstance(pykka.__version__, basestring)
+        and not SV('1.0') <= SV(pykka.__version__) < SV('2.0')):
+    sys.exit(
+        u'Mopidy requires Pykka >= 1.0, < 2, but found %s' % pykka.__version__)
 
 import os
 import platform
