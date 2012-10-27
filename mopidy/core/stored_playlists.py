0641d2d2074cc4e7da80e13410ada5387ded7421
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-10-27 23:07:19 +0200

    Make core.stored_playlists.playlists support multiple backends

--- mopidy/core/stored_playlists.py
+++ mopidy/core/stored_playlists.py
@@ -1,3 +1,6 @@
+import pykka
+
+
 class StoredPlaylistsController(object):
     pykka_traversable = True
 
@@ -12,10 +15,14 @@ class StoredPlaylistsController(object):
 
         Read/write. List of :class:`mopidy.models.Playlist`.
         """
-        return self.backends[0].stored_playlists.playlists.get()
+        futures = [backend.stored_playlists.playlists
+            for backend in self.backends]
+        results = pykka.get_all(futures)
+        return [playlist for result in results for playlist in result]
 
     @playlists.setter  # noqa
     def playlists(self, playlists):
+        # TODO Support multiple backends
         self.backends[0].stored_playlists.playlists = playlists
 
     def create(self, name):
@@ -26,6 +33,7 @@ class StoredPlaylistsController(object):
         :type name: string
         :rtype: :class:`mopidy.models.Playlist`
         """
+        # TODO Support multiple backends
         return self.backends[0].stored_playlists.create(name).get()
 
     def delete(self, playlist):
@@ -35,6 +43,7 @@ class StoredPlaylistsController(object):
         :param playlist: the playlist to delete
         :type playlist: :class:`mopidy.models.Playlist`
         """
+        # TODO Support multiple backends
         return self.backends[0].stored_playlists.delete(playlist).get()
 
     def get(self, **criteria):
@@ -76,12 +85,14 @@ class StoredPlaylistsController(object):
         :type uri: string
         :rtype: :class:`mopidy.models.Playlist`
         """
+        # TODO Support multiple backends
         return self.backends[0].stored_playlists.lookup(uri).get()
 
     def refresh(self):
         """
         Refresh the stored playlists in :attr:`playlists`.
         """
+        # TODO Support multiple backends
         return self.backends[0].stored_playlists.refresh().get()
 
     def rename(self, playlist, new_name):
@@ -93,6 +104,7 @@ class StoredPlaylistsController(object):
         :param new_name: the new name
         :type new_name: string
         """
+        # TODO Support multiple backends
         return self.backends[0].stored_playlists.rename(
             playlist, new_name).get()
 
@@ -103,4 +115,5 @@ class StoredPlaylistsController(object):
         :param playlist: the playlist
         :type playlist: :class:`mopidy.models.Playlist`
         """
+        # TODO Support multiple backends
         return self.backends[0].stored_playlists.save(playlist).get()
