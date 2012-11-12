a2d7f2f504f36a1548086b767398131ad8501903
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-11-12 23:11:32 +0100

    spotify: Update stored playlists interface

--- mopidy/backends/spotify/stored_playlists.py
+++ mopidy/backends/spotify/stored_playlists.py
@@ -5,16 +5,13 @@ class SpotifyStoredPlaylistsProvider(base.BaseStoredPlaylistsProvider):
     def create(self, name):
         pass  # TODO
 
-    def delete(self, playlist):
+    def delete(self, uri):
         pass  # TODO
 
     def lookup(self, uri):
         pass  # TODO
 
     def refresh(self):
-        pass  # TODO
-
-    def rename(self, playlist, new_name):
         pass  # TODO
 
     def save(self, playlist):
