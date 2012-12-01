ac6cecd2f87f543de23da03e8e6af326de859045
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-12-01 11:21:06 +0100

    backends: Add BackendListener.on_event()

--- tests/backends/listener_test.py
+++ tests/backends/listener_test.py
@@ -1,13 +1,22 @@
 from __future__ import unicode_literals
 
+import mock
+
 from mopidy.backends.listener import BackendListener
 
 from tests import unittest
 
 
-class CoreListenerTest(unittest.TestCase):
+class BackendListenerTest(unittest.TestCase):
     def setUp(self):
         self.listener = BackendListener()
+
+    def test_on_event_forwards_to_specific_handler(self):
+        self.listener.playlists_loaded = mock.Mock()
+
+        self.listener.on_event('playlists_loaded')
+
+        self.listener.playlists_loaded.assert_called_with()
 
     def test_listener_has_default_impl_for_playlists_loaded(self):
         self.listener.playlists_loaded()
