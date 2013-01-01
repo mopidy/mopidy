9314f2df5c6b434982df7a0156b1e5257fd07000
Thomas Adamcik <thomas@adamcik.no>
2013-01-01 17:25:07 +0100

    audio: Move supported URI checking to mopidy.audio.utils
    In order to avoid gstreamer imports leaking into more of our code I'm moving
    this to a new utils class in audio.

---
+++ mopidy/audio/utils.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+import pygst
+pygst.require('0.10')
+import gst
+
+
+def supported_uri_schemes(uri_schemes):
+    """Determine which URIs we can actually support from provided whitelist.
+
+    :param uri_schemes: list/set of URIs to check support for.
+    :type uri_schemes: list or set or URI schemes as strings.
+    :rtype: set of URI schemes we can support via this GStreamer install.
+    """
+    supported_schemes= set()
+    registry = gst.registry_get_default()
+
+    for factory in registry.get_feature_list(gst.TYPE_ELEMENT_FACTORY):
+        for uri in factory.get_uri_protocols():
+            if uri in uri_schemes:
+                supported_schemes.add(uri)
+
+    return supported_schemes
