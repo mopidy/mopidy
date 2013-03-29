74b4fdc7ee471ad9ac477c719fc6ea2dcc5a6019
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-03-29 13:51:28 +0100

    js: Make test suite run on Node.js using faye-websocket

--- js/buster.js
+++ js/buster.js
@@ -1,7 +1,15 @@
 var config = module.exports;
 
-config["tests"] = {
+config.browser_tests = {
     environment: "browser",
+    libs: ["lib/**/*.js"],
+    sources: ["src/**/*.js"],
+    testHelpers: ["test/**/*-helper.js"],
+    tests: ["test/**/*-test.js"]
+};
+
+config.node_tests = {
+    environment: "node",
     libs: ["lib/**/*.js"],
     sources: ["src/**/*.js"],
     testHelpers: ["test/**/*-helper.js"],
