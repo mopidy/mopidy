6822f1248d9a34d87091706cc62fc49ddaa6150b
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-04-03 10:38:04 +0200

    spotify: Switch to new config system

--- mopidy/backends/spotify/actor.py
+++ mopidy/backends/spotify/actor.py
@@ -4,23 +4,20 @@ import logging
 
 import pykka
 
-from mopidy import settings
 from mopidy.backends import base
+from mopidy.backends.spotify.library import SpotifyLibraryProvider
+from mopidy.backends.spotify.playback import SpotifyPlaybackProvider
+from mopidy.backends.spotify.session_manager import SpotifySessionManager
+from mopidy.backends.spotify.playlists import SpotifyPlaylistsProvider
 
 logger = logging.getLogger('mopidy.backends.spotify')
 
 
 class SpotifyBackend(pykka.ThreadingActor, base.Backend):
-    # Imports inside methods are to prevent loading of __init__.py to fail on
-    # missing spotify dependencies.
-
     def __init__(self, config, audio):
         super(SpotifyBackend, self).__init__()
 
-        from .library import SpotifyLibraryProvider
-        from .playback import SpotifyPlaybackProvider
-        from .session_manager import SpotifySessionManager
-        from .playlists import SpotifyPlaylistsProvider
+        self.config = config
 
         self.library = SpotifyLibraryProvider(backend=self)
         self.playback = SpotifyPlaybackProvider(audio=audio, backend=self)
@@ -28,17 +25,8 @@ class SpotifyBackend(pykka.ThreadingActor, base.Backend):
 
         self.uri_schemes = ['spotify']
 
-        # Fail early if settings are not present
-        username = settings.SPOTIFY_USERNAME
-        password = settings.SPOTIFY_PASSWORD
-        proxy = settings.SPOTIFY_PROXY_HOST
-        proxy_username = settings.SPOTIFY_PROXY_USERNAME
-        proxy_password = settings.SPOTIFY_PROXY_PASSWORD
-
         self.spotify = SpotifySessionManager(
-            username, password, audio=audio, backend_ref=self.actor_ref,
-            proxy=proxy, proxy_username=proxy_username,
-            proxy_password=proxy_password)
+            config, audio=audio, backend_ref=self.actor_ref)
 
     def on_start(self):
         logger.info('Mopidy uses SPOTIFY(R) CORE')
