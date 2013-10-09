c3e88993964f1c775829fc2f14aea2e84d33c099
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-10-09 23:28:01 +0200

    mpd: Test that output enabling/disabling unmutes/mutes audio

--- tests/frontends/mpd/protocol/audio_output_test.py
+++ tests/frontends/mpd/protocol/audio_output_test.py
@@ -5,16 +5,37 @@ from tests.frontends.mpd import protocol
 
 class AudioOutputHandlerTest(protocol.BaseTestCase):
     def test_enableoutput(self):
+        self.core.playback.mute = True
+
         self.sendRequest('enableoutput "0"')
+
         self.assertInResponse('OK')
+        self.assertEqual(self.core.playback.mute.get(), False)
 
     def test_disableoutput(self):
-        self.sendRequest('disableoutput "0"')
-        self.assertInResponse('OK')
+        self.core.playback.mute = False
 
-    def test_outputs(self):
+        self.sendRequest('disableoutput "0"')
+
+        self.assertInResponse('OK')
+        self.assertEqual(self.core.playback.mute.get(), True)
+
+    def test_outputs_when_unmuted(self):
+        self.core.playback.mute = False
+
         self.sendRequest('outputs')
+
         self.assertInResponse('outputid: 0')
         self.assertInResponse('outputname: Default')
         self.assertInResponse('outputenabled: 1')
+        self.assertInResponse('OK')
+
+    def test_outputs_when_muted(self):
+        self.core.playback.mute = True
+
+        self.sendRequest('outputs')
+
+        self.assertInResponse('outputid: 0')
+        self.assertInResponse('outputname: Default')
+        self.assertInResponse('outputenabled: 0')
         self.assertInResponse('OK')
