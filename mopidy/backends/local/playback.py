f8dedc0b848a7fdce6caf360279b5a450729dcc1
Thomas Adamcik <thomas@adamcik.no>
2013-11-06 21:46:41 +0100

    local: Add local_to_file_uri translator

--- mopidy/backends/local/playback.py
+++ mopidy/backends/local/playback.py
@@ -6,14 +6,14 @@ import os
 from mopidy.backends import base
 from mopidy.utils import path
 
+from . import translator
+
 logger = logging.getLogger('mopidy.backends.local')
 
 
 class LocalPlaybackProvider(base.BasePlaybackProvider):
     def change_track(self, track):
         media_dir = self.backend.config['local']['media_dir']
-        # TODO: check that type is correct.
-        file_path = path.uri_to_path(track.uri).split(b':', 1)[1]
-        file_path = os.path.join(media_dir, file_path)
-        track = track.copy(uri=path.path_to_uri(file_path))
+        uri = translator.local_to_file_uri(track.uri, media_dir)
+        track = track.copy(uri=uri)
         return super(LocalPlaybackProvider, self).change_track(track)
