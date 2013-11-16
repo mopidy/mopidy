34e327ed127440241f60fb48a1b7870961950440
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-11-16 01:28:44 +0100

    zeroconf: Wrap publish in try-except

--- mopidy/utils/zeroconf.py
+++ mopidy/utils/zeroconf.py
@@ -46,33 +46,32 @@ class Zeroconf(object):
 
         try:
             bus = dbus.SystemBus()
-        except dbus.exceptions.DBusException as e:
-            logger.debug('Zeroconf publish failed: %s', e)
-            return False
 
-        if not bus.name_has_owner('org.freedesktop.Avahi'):
-            logger.debug('Zeroconf publish failed: Avahi service not running.')
-            return False
+            if not bus.name_has_owner('org.freedesktop.Avahi'):
+                logger.debug(
+                    'Zeroconf publish failed: Avahi service not running.')
+                return False
 
-        server = dbus.Interface(bus.get_object('org.freedesktop.Avahi', '/'),
-                                'org.freedesktop.Avahi.Server')
+            server = dbus.Interface(
+                bus.get_object('org.freedesktop.Avahi', '/'),
+                'org.freedesktop.Avahi.Server')
 
-        self.group = dbus.Interface(
-            bus.get_object('org.freedesktop.Avahi', server.EntryGroupNew()),
-            'org.freedesktop.Avahi.EntryGroup')
+            self.group = dbus.Interface(
+                bus.get_object(
+                    'org.freedesktop.Avahi', server.EntryGroupNew()),
+                'org.freedesktop.Avahi.EntryGroup')
 
-        try:
             text = [_convert_text_to_dbus_bytes(t) for t in self.text]
             self.group.AddService(
                 _AVAHI_IF_UNSPEC, _AVAHI_PROTO_UNSPEC,
                 dbus.UInt32(_AVAHI_PUBLISHFLAGS_NONE), self.name, self.stype,
                 self.domain, self.host, dbus.UInt16(self.port), text)
+
+            self.group.Commit()
+            return True
         except dbus.exceptions.DBusException as e:
             logger.debug('Zeroconf publish failed: %s', e)
             return False
-
-        self.group.Commit()
-        return True
 
     def unpublish(self):
         if self.group:
