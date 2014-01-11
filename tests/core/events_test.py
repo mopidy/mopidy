d724001f5b692e7ddc28563d0629a0d992d35452
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-01-11 15:49:22 +0100

    tests: Move mopidy.backends.dummy to tests.dummy_backend

--- tests/core/events_test.py
+++ tests/core/events_test.py
@@ -6,14 +6,15 @@ import unittest
 import pykka
 
 from mopidy import core
-from mopidy.backends import dummy
 from mopidy.models import Track
+
+from tests import dummy_backend
 
 
 @mock.patch.object(core.CoreListener, 'send')
 class BackendEventsTest(unittest.TestCase):
     def setUp(self):
-        self.backend = dummy.create_dummy_backend_proxy()
+        self.backend = dummy_backend.create_dummy_backend_proxy()
         self.core = core.Core.start(backends=[self.backend]).proxy()
 
     def tearDown(self):
