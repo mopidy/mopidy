e4869e9e348dd5a0441edc0aca7c0298c2c892a9
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-01-18 02:12:04 +0100

    backend: Make old DummyBackend imports work

---
+++ mopidy/backends/dummy.py
@@ -0,0 +1,5 @@
+from __future__ import unicode_literals
+
+# Make classes previously residing here available in the old location for
+# backwards compatibility with extensions targeting Mopidy < 0.18.
+from mopidy.backend.dummy import *  # noqa
