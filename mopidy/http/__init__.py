a318fd8698b23c0e0c66297d0057c49ce533b57a
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-05-20 22:27:44 +0200

    http: Improve router attribute docstrings

--- mopidy/http/__init__.py
+++ mopidy/http/__init__.py
@@ -60,11 +60,27 @@ class Router(object):
     :param config: dict structure of the entire Mopidy configuration
     """
 
-    #: Name of the HTTP router implementation, must be overridden.
     name = None
+    """Name of the HTTP router.
 
-    #: Path to location of static files to be served.
+    Must be overridden by all subclasses.
+
+    This should be the same as the ``ext_name`` of the Mopidy extension
+    implementing an HTTP router. The :attr:`name` will be used to namespace all
+    URLs handled by this router.
+
+    For example, if :attr:`name` is ``soundspot``, then the router will manage
+    all requests starting with ``http://localhost:6680/soundspot``.
+    """
+
     static_file_path = None
+    """Path to location of static files to be served.
+
+    If you only need to serve static files, set this attribute and use the
+    default implementation of :meth:`get_request_handlers`.
+
+    If you override :meth:`get_request_handlers` this attribute is not used.
+    """
 
     def __init__(self, config):
         self.config = config
