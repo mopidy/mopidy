19dae1057549ee7c9e2c5e59c1b570a20cbc863e
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-07-23 18:14:00 +0200

    Bump version to 0.19.1

--- tests/test_version.py
+++ tests/test_version.py
@@ -46,5 +46,6 @@ class VersionTest(unittest.TestCase):
         self.assertLess(SV('0.18.0'), SV('0.18.1'))
         self.assertLess(SV('0.18.1'), SV('0.18.2'))
         self.assertLess(SV('0.18.2'), SV('0.18.3'))
-        self.assertLess(SV('0.18.3'), SV(__version__))
-        self.assertLess(SV(__version__), SV('0.19.1'))
+        self.assertLess(SV('0.18.3'), SV('0.19.0'))
+        self.assertLess(SV('0.19.0'), SV(__version__))
+        self.assertLess(SV(__version__), SV('0.19.2'))
