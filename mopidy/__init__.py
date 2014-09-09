37c736533dbd0f771be4df6bb49c19be01ed8f18
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-09-09 08:38:12 +0200

    Remove Pykka version check
    This check was made redundant with our move to setuptools. The executables
    made by setuptools checks if all our Python dependencies are available in the
    required versions before starting Mopidy.

--- mopidy/__init__.py
+++ mopidy/__init__.py
@@ -4,21 +4,12 @@ import platform
 import sys
 import textwrap
 import warnings
-from distutils.version import StrictVersion as SV
-
-import pykka
 
 
 if not (2, 7) <= sys.version_info < (3,):
     sys.exit(
         'ERROR: Mopidy requires Python 2.7, but found %s.' %
         platform.python_version())
-
-if (isinstance(pykka.__version__, basestring)
-        and not SV('1.1') <= SV(pykka.__version__) < SV('2.0')):
-    sys.exit(
-        'ERROR: Mopidy requires Pykka >= 1.1, < 2, but found %s.' %
-        pykka.__version__)
 
 try:
     import gobject   # noqa
