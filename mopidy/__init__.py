2050385a5f5fdcffe333ae17463d6469af0b5cd8
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-09-09 08:24:42 +0200

    Update Python and Pykka version check error messages

--- mopidy/__init__.py
+++ mopidy/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import platform
 import sys
 import warnings
 from distutils.version import StrictVersion as SV
@@ -9,13 +10,14 @@ import pykka
 
 if not (2, 7) <= sys.version_info < (3,):
     sys.exit(
-        'Mopidy requires Python >= 2.7, < 3, but found %s' %
-        '.'.join(map(str, sys.version_info[:3])))
+        'ERROR: Mopidy requires Python 2.7, but found %s.' %
+        platform.python_version())
 
 if (isinstance(pykka.__version__, basestring)
         and not SV('1.1') <= SV(pykka.__version__) < SV('2.0')):
     sys.exit(
-        'Mopidy requires Pykka >= 1.1, < 2, but found %s' % pykka.__version__)
+        'ERROR: Mopidy requires Pykka >= 1.1, < 2, but found %s.' %
+        pykka.__version__)
 
 
 warnings.filterwarnings('ignore', 'could not open display')
