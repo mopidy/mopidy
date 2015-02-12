07f2d46ccfdfbf1ca51f91c6dc9a1f13495c453d
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-02-12 23:49:47 +0100

    softwaremixer: Update comment after audio refactoring

--- mopidy/softwaremixer/mixer.py
+++ mopidy/softwaremixer/mixer.py
@@ -28,9 +28,9 @@ class SoftwareMixer(pykka.ThreadingActor, mixer.Mixer):
         self._audio_mixer = mixer_ref
 
         # The Mopidy startup procedure will set the initial volume of a
-        # mixer, but this happens before the audio actor is injected into the
-        # software mixer and has no effect. Thus, we need to set the initial
-        # volume again.
+        # mixer, but this happens before the audio actor's mixer is injected
+        # into the software mixer actor and has no effect. Thus, we need to set
+        # the initial volume again.
         if self._initial_volume is not None:
             self.set_volume(self._initial_volume)
         if self._initial_mute is not None:
