05f26c1af09cbc08228cb1fade1d5c5fc6424b8d
Thomas Kemmer <tkemmer@computer.org>
2015-02-23 17:36:31 +0100

    Emit event on playlist delete.

--- mopidy/local/playlists.py
+++ mopidy/local/playlists.py
@@ -43,6 +43,8 @@ class LocalPlaylistsProvider(backend.PlaylistsProvider):
         except OSError as e:
             logger.error('Error deleting playlist %s: %s', uri, e)
         self._playlists.remove(playlist)
+        # TODO: handle in PlaylistsController, playlist_changed?
+        backend.BackendListener.send('playlists_loaded')
 
     def lookup(self, uri):
         # TODO: store as {uri: playlist}?
