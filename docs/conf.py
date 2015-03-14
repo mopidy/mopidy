a1e866e46e88e1822f918615e4e671a9129cd3fb
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-03-14 22:06:38 +0100

    docs: Use sphinx_rtd_theme bundled with Sphinx 1.3

--- docs/conf.py
+++ docs/conf.py
@@ -114,7 +114,7 @@ modindex_common_prefix = ['mopidy.']
 
 # -- Options for HTML output --------------------------------------------------
 
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 html_theme_path = ['_themes']
 html_static_path = ['_static']
 
