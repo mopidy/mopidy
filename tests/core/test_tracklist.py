2bc3db0d0e95e8f5ba90f5fa39b616009427a0f6
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-03-21 00:16:22 +0100

    core: Add uris kwarg to tracklist.core()
    Fixes #1060

--- tests/core/test_tracklist.py
+++ tests/core/test_tracklist.py
@@ -26,8 +26,7 @@ class TracklistTest(unittest.TestCase):
 
     def test_add_by_uri_looks_up_uri_in_library(self):
         track = Track(uri='dummy1:x', name='x')
-        self.library.lookup().get.return_value = [track]
-        self.library.lookup.reset_mock()
+        self.library.lookup.return_value.get.return_value = [track]
 
         tl_tracks = self.core.tracklist.add(uri='dummy1:x')
 
@@ -35,6 +34,26 @@ class TracklistTest(unittest.TestCase):
         self.assertEqual(1, len(tl_tracks))
         self.assertEqual(track, tl_tracks[0].track)
         self.assertEqual(tl_tracks, self.core.tracklist.tl_tracks[-1:])
+
+    def test_add_by_uris_looks_up_uris_in_library(self):
+        track1 = Track(uri='dummy1:x', name='x')
+        track2 = Track(uri='dummy1:y1', name='y1')
+        track3 = Track(uri='dummy1:y2', name='y2')
+        self.library.lookup.return_value.get.side_effect = [
+            [track1], [track2, track3]]
+
+        tl_tracks = self.core.tracklist.add(uris=['dummy1:x', 'dummy1:y'])
+
+        self.library.lookup.assert_has_calls([
+            mock.call('dummy1:x'),
+            mock.call('dummy1:y'),
+        ])
+        self.assertEqual(3, len(tl_tracks))
+        self.assertEqual(track1, tl_tracks[0].track)
+        self.assertEqual(track2, tl_tracks[1].track)
+        self.assertEqual(track3, tl_tracks[2].track)
+        self.assertEqual(
+            tl_tracks, self.core.tracklist.tl_tracks[-len(tl_tracks):])
 
     def test_remove_removes_tl_tracks_matching_query(self):
         tl_tracks = self.core.tracklist.remove(name=['foo'])
