f36110983241853eb0f257203f74085a6edccf3a
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-03-22 09:23:07 +0100

    setup: Explicitly close file
    Instead of relying on GC.

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ from setuptools import find_packages, setup
 
 
 def get_version(filename):
-    init_py = open(filename).read()
-    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
-    return metadata['version']
+    with open(filename) as fh:
+        metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", fh.read()))
+        return metadata['version']
 
 
 setup(
