3810089be39a4301cad31445ba4bdab84dbc3375
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-07-27 13:41:29 +0200

    tests: Avoid import errors during conftest setup

--- tests/file/conftest.py
+++ tests/file/conftest.py
@@ -2,8 +2,6 @@ from __future__ import unicode_literals
 
 import pytest
 
-from mopidy.file import library
-
 
 @pytest.fixture
 def file_config():
@@ -15,4 +13,8 @@ def file_config():
 
 @pytest.fixture
 def file_library(file_config):
+    # Import library, thus scanner, thus gobject as late as possible to avoid
+    # hard to track import errors during conftest setup.
+    from mopidy.file import library
+
     return library.FileLibraryProvider(backend=None, config=file_config)
