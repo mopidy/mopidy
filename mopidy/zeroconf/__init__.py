248bd2fe8f67f0259f1a09ace04f08660d19331c
tmyersjstar <tmyers@jstar.ai>
2016-03-30 20:13:52 -0600

    Add zeroconf support for OSX via pybonjour.

---
+++ mopidy/zeroconf/__init__.py
@@ -0,0 +1,55 @@
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+def _is_loopback_address(host):
+    return (
+        host.startswith('127.') or
+        host.startswith('::ffff:127.') or
+        host == '::1')
+
+
+class ZeroconfInterface(object):
+
+    """Base class for Zeroconf implementations"""
+
+    """Publish a network service with Zeroconf.
+
+    :param str name: human readable name of the service, e.g. 'MPD on neptune'
+    :param str stype: service type, e.g. '_mpd._tcp'
+    :param int port: TCP port of the service, e.g. 6600
+    :param str domain: local network domain name, defaults to ''
+    :param str host: interface to advertise the service on, defaults to ''
+    :param text: extra information depending on ``stype``, defaults to empty
+        list
+    :type text: list of str
+    """
+    def __init__(self, name, stype, port, domain='', host='', text=None):
+        self.stype = stype
+        self.port = port
+        self.domain = domain
+        self.host = host
+        self.text = text or []
+
+        self.group = None
+        self.display_hostname = None
+        self.name = None
+
+    def __str__(self):
+        return 'Zeroconf service "%s" (%s at [%s]:%d)' % (
+            self.name, self.stype, self.host, self.port)
+
+    def publish(self):
+        raise NotImplementedError
+
+    def unpublish(self):
+        raise NotImplementedError
+
+
+if sys.platform == 'darwin':
+    logger.debug('Detected OSX, using alternative bonjour impl.')
+    from .pybonjour import Zeroconf
+else:
+    from .avahi import Zeroconf
---
+++ mopidy/zeroconf/pybonjour.py
@@ -0,0 +1,80 @@
+from __future__ import absolute_import, unicode_literals
+
+import logging
+import select
+import string
+
+from mopidy.zeroconf import ZeroconfInterface, _is_loopback_address
+
+logger = logging.getLogger(__name__)
+
+try:
+    import pybonjour
+except ImportError:
+    pybonjour = None
+
+
+class Zeroconf(ZeroconfInterface):
+
+    """Publish a network service with Zeroconf using pybonjour
+
+    :param str name: human readable name of the service, e.g. 'MPD on neptune'
+    :param str stype: service type, e.g. '_mpd._tcp'
+    :param int port: TCP port of the service, e.g. 6600
+    :param str domain: local network domain name, defaults to ''
+    :param str host: interface to advertise the service on, defaults to ''
+    :param text: extra information depending on ``stype``, defaults to empty
+        list
+    :type text: list of str
+    """
+
+    def __init__(self, name, stype, port, domain='', host='', text=None):
+        ZeroconfInterface.__init__(self, name, stype, port, domain, host, text)
+
+        if pybonjour:
+            self.name = string.Template(name).safe_substitute(
+                hostname=self.display_hostname, port=port)
+
+    def register_callback(self, sdRef, flags, errorCode, name, regtype, domain):
+        if errorCode == pybonjour.kDNSServiceErr_NoError:
+            logger.debug(
+                    '%s: Registered service: name = %s, regtype = %s, domain = %s', self, name, regtype, domain)
+
+    def publish(self):
+        """Publish the service.
+
+        Call when your service starts.
+        """
+
+        if _is_loopback_address(self.host):
+            logger.debug(
+                '%s: Publish on loopback interface is not supported.', self)
+            return False
+
+        if not pybonjour:
+            logger.debug('%s: pybonjour not installed; publish failed.', self)
+            return False
+
+        sdRef = pybonjour.DNSServiceRegister(
+            name=self.name,
+            regtype=self.stype, port=self.port,
+            domain=self.domain,
+            callBack=self.register_callback)
+
+        processed = False
+        while not processed:
+            ready = select.select([sdRef], [], [])
+            if sdRef in ready[0]:
+                pybonjour.DNSServiceProcessResult(sdRef)
+                processed = True
+
+        logger.debug('%s: Published', self)
+        return True
+
+    def unpublish(self):
+        """Unpublish the service.
+
+        Call when your service shuts down.
+        """
+
+        logger.debug('%s: Unpublished', self)
