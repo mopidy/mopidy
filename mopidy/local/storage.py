cd6bfeb68105e58e6e4fe6542f8fc126363b6015
Aleksandar Benic <aleksandar.benic@protonmail.com>
2016-10-23 13:33:07 +0200

    Fix formatting

--- mopidy/local/storage.py
+++ mopidy/local/storage.py
@@ -9,6 +9,5 @@ logger = logging.getLogger(__name__)
 def check_dirs_and_files(config):
     if not os.path.isdir(config['local']['media_dir']):
         logger.warning(
-            'Local media dir %s does not exist or we lack \
-                    permissions to the directory or one of its parents' %
-            config['local']['media_dir'])
+            'Local media dir %s does not exist or we lack permissions to the '
+            'directory or one of its parents' % config['local']['media_dir'])
