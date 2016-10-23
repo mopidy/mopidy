4ddb225955c5672d76c93936f8535169c14aec35
Aleksandar Benic <aleksandar.benic@protonmail.com>
2016-10-23 12:55:05 +0200

    Lapsus calami

--- mopidy/local/storage.py
+++ mopidy/local/storage.py
@@ -10,5 +10,5 @@ def check_dirs_and_files(config):
     if not os.path.isdir(config['local']['media_dir']):
         logger.warning(
             'Local media dir %s does not exist or we lack \
-                    permissions to the directory or one of it\'s parents' %
+                    permissions to the directory or one of its parents' %
             config['local']['media_dir'])
