9bf8ac2099412d716ae89a9b0509b61394edeffc
Stein Magnus Jodal <stein.magnus@jodal.no>
2018-11-26 22:33:59 +0100

    xdg: Read paths from user-dirs.dirs as bytes
    Fixes #1676, #1725

--- tests/internal/test_xdg.py
+++ tests/internal/test_xdg.py
@@ -21,9 +21,10 @@ def test_cache_dir_default(environ):
 
 
 def test_cache_dir_from_env(environ):
-    os.environ['XDG_CACHE_HOME'] = '/foo/bar'
+    os.environ['XDG_CACHE_HOME'] = b'/foo/bar'
 
-    assert xdg.get_dirs()['XDG_CACHE_DIR'] == '/foo/bar'
+    assert xdg.get_dirs()['XDG_CACHE_DIR'] == b'/foo/bar'
+    assert type(xdg.get_dirs()['XDG_CACHE_DIR']) == bytes
 
 
 def test_config_dir_default(environ):
@@ -31,9 +32,10 @@ def test_config_dir_default(environ):
 
 
 def test_config_dir_from_env(environ):
-    os.environ['XDG_CONFIG_HOME'] = '/foo/bar'
+    os.environ['XDG_CONFIG_HOME'] = b'/foo/bar'
 
-    assert xdg.get_dirs()['XDG_CONFIG_DIR'] == '/foo/bar'
+    assert xdg.get_dirs()['XDG_CONFIG_DIR'] == b'/foo/bar'
+    assert type(xdg.get_dirs()['XDG_CONFIG_DIR']) == bytes
 
 
 def test_data_dir_default(environ):
@@ -42,21 +44,23 @@ def test_data_dir_default(environ):
 
 
 def test_data_dir_from_env(environ):
-    os.environ['XDG_DATA_HOME'] = '/foo/bar'
+    os.environ['XDG_DATA_HOME'] = b'/foo/bar'
 
-    assert xdg.get_dirs()['XDG_DATA_DIR'] == '/foo/bar'
+    assert xdg.get_dirs()['XDG_DATA_DIR'] == b'/foo/bar'
+    assert type(xdg.get_dirs()['XDG_DATA_DIR']) == bytes
 
 
 def test_user_dirs(environ, tmpdir):
     os.environ['XDG_CONFIG_HOME'] = str(tmpdir)
 
     with open(os.path.join(str(tmpdir), 'user-dirs.dirs'), 'wb') as fh:
-        fh.write('# Some comments\n')
-        fh.write('XDG_MUSIC_DIR="$HOME/Music2"\n')
+        fh.write(b'# Some comments\n')
+        fh.write(b'XDG_MUSIC_DIR="$HOME/Music2"\n')
 
     result = xdg.get_dirs()
 
     assert result['XDG_MUSIC_DIR'] == os.path.expanduser(b'~/Music2')
+    assert type(result['XDG_MUSIC_DIR']) == bytes
     assert 'XDG_DOWNLOAD_DIR' not in result
 
 
