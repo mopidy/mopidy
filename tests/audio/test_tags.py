9a4dd1e960fe8d69618e20e479eacc4f3ee766cc
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-06-09 15:34:23 +0200

    audio: Fix warning by not initing GValue twice

--- tests/audio/test_tags.py
+++ tests/audio/test_tags.py
@@ -27,8 +27,6 @@ class TestConvertTaglist(object):
             elif isinstance(value, int):
                 gobject_value.init(GObject.TYPE_UINT)
                 gobject_value.set_uint(value)
-                gobject_value.init(GObject.TYPE_VALUE)
-                gobject_value.set_value(value)
             else:
                 raise TypeError
             taglist.add_value(Gst.TagMergeMode.APPEND, tag, gobject_value)
