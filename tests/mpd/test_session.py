mopidy/mopidy

commit b4c98ec4a54fab4dc58182abf32b020d7295d743

Fix/1531 add unix domain socket (#1629)
mpd: add functionality for unix domain socket (Fixes #1531)

The Hostname config type now supports a Unix socket path prefixed with `unix:`

mopidy/internal/network.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 import errno
 import logging
+import os
 import re
 import socket
 import sys
@@ -9,11 +10,18 @@ import threading
 
 import pykka
 
-from mopidy.internal import encoding
+from mopidy.internal import encoding, path, validation
 from mopidy.internal.gi import GObject
 
 
 logger = logging.getLogger(__name__)
+
+
+def is_unix_socket(sock):
+    """Check if the provided socket is a Unix domain socket"""
+    if hasattr(socket, 'AF_UNIX'):
+        return sock.family == socket.AF_UNIX
+    return False
 
 
 class ShouldRetrySocketCall(Exception):
@@ -40,7 +48,7 @@ def try_ipv6_socket():
 has_ipv6 = try_ipv6_socket()
 
 
-def create_socket():
+def create_tcp_socket():
     """Create a TCP socket with or without IPv6 depending on system support"""
     if has_ipv6:
         sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
@@ -55,6 +63,19 @@ def create_socket():
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     return sock
+
+
+def create_unix_socket():
+    """Create a Unix domain socket"""
+    return socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+
+def format_socket_name(sock):
+    """Format the connection string for the given socket"""
+    if is_unix_socket(sock):
+        return '%s' % sock.getsockname()
+    else:
+        return '[%s]:%s' % sock.getsockname()[:2]
 
 
 def format_hostname(hostname):
@@ -76,17 +97,42 @@ class Server(object):
         self.timeout = timeout
         self.server_socket = self.create_server_socket(host, port)
 
-        self.register_server_socket(self.server_socket.fileno())
+        self.watcher = self.register_server_socket(self.server_socket.fileno())
 
     def create_server_socket(self, host, port):
-        sock = create_socket()
+        socket_path = path.get_unix_socket_path(host)
+        if socket_path is not None:  # host is a path so use unix socket
+            sock = create_unix_socket()
+            sock.bind(socket_path)
+        else:
+            # ensure the port is supplied
+            validation.check_integer(port)
+            sock = create_tcp_socket()
+            sock.bind((host, port))
+
         sock.setblocking(False)
-        sock.bind((host, port))
         sock.listen(1)
         return sock
 
+    def stop(self):
+        GObject.source_remove(self.watcher)
+        if is_unix_socket(self.server_socket):
+            unix_socket_path = self.server_socket.getsockname()
+        else:
+            unix_socket_path = None
+
+        self.server_socket.shutdown(socket.SHUT_RDWR)
+        self.server_socket.close()
+
+        # clean up the socket file
+        if unix_socket_path is not None:
+            os.unlink(unix_socket_path)
+
     def register_server_socket(self, fileno):
-        GObject.io_add_watch(fileno, GObject.IO_IN, self.handle_connection)
+        return GObject.io_add_watch(
+            fileno,
+            GObject.IO_IN,
+            self.handle_connection)
 
     def handle_connection(self, fd, flags):
         try:
@@ -102,7 +148,10 @@ class Server(object):
 
     def accept_connection(self):
         try:
-            return self.server_socket.accept()
+            sock, addr = self.server_socket.accept()
+            if is_unix_socket(sock):
+                addr = (sock.getsockname(), None)
+            return sock, addr
         except socket.error as e:
             if e.errno in (errno.EAGAIN, errno.EINTR):
                 raise ShouldRetrySocketCall
@@ -117,7 +166,9 @@ class Server(object):
 
     def reject_connection(self, sock, addr):
         # FIXME provide more context in logging?
-        logger.warning('Rejected connection from [%s]:%s', addr[0], addr[1])
+        logger.warning(
+            'Rejected connection from %s',
+            format_socket_name(sock))
         try:
             sock.close()
         except socket.error:
@@ -142,7 +193,7 @@ class Connection(object):
 
         self.host, self.port = addr[:2]  # IPv6 has larger addr
 
-        self.sock = sock
+        self._sock = sock
         self.protocol = protocol
         self.protocol_kwargs = protocol_kwargs
         self.timeout = timeout
@@ -180,7 +231,7 @@ class Connection(object):
         self.disable_send()
 
         try:
-            self.sock.close()
+            self._sock.close()
         except socket.error:
             pass
 
@@ -195,7 +246,7 @@ class Connection(object):
     def send(self, data):
         """Send data to client, return any unsent data."""
         try:
-            sent = self.sock.send(data)
+            sent = self._sock.send(data)
             return data[sent:]
         except socket.error as e:
             if e.errno in (errno.EWOULDBLOCK, errno.EINTR):
@@ -226,7 +277,7 @@ class Connection(object):
 
         try:
             self.recv_id = GObject.io_add_watch(
-                self.sock.fileno(),
+                self._sock.fileno(),
                 GObject.IO_IN | GObject.IO_ERR | GObject.IO_HUP,
                 self.recv_callback)
         except socket.error as e:
@@ -244,7 +295,7 @@ class Connection(object):
 
         try:
             self.send_id = GObject.io_add_watch(
-                self.sock.fileno(),
+                self._sock.fileno(),
                 GObject.IO_OUT | GObject.IO_ERR | GObject.IO_HUP,
                 self.send_callback)
         except socket.error as e:
@@ -263,7 +314,7 @@ class Connection(object):
             return True
 
         try:
-            data = self.sock.recv(4096)
+            data = self._sock.recv(4096)
         except socket.error as e:
             if e.errno not in (errno.EWOULDBLOCK, errno.EINTR):
                 self.stop('Unexpected client error: %s' % e)
@@ -303,6 +354,9 @@ class Connection(object):
     def timeout_callback(self):
         self.stop('Client inactive for %ds; closing connection' % self.timeout)
         return False
+
+    def __str__(self):
+        return format_socket_name(self._sock)
 
 
 class LineProtocol(pykka.ThreadingActor):

mopidy/mpd/actor.py
@@ -41,11 +41,11 @@ class MpdFrontend(pykka.ThreadingActor, CoreListener):
         self.zeroconf_name = config['mpd']['zeroconf']
         self.zeroconf_service = None
 
-        self._setup_server(config, core)
+        self.server = self._setup_server(config, core)
 
     def _setup_server(self, config, core):
         try:
-            network.Server(
+            server = network.Server(
                 self.hostname, self.port,
                 protocol=session.MpdSession,
                 protocol_kwargs={
@@ -60,10 +60,15 @@ class MpdFrontend(pykka.ThreadingActor, CoreListener):
                 'MPD server startup failed: %s' %
                 encoding.locale_decode(error))
 
-        logger.info('MPD server running at [%s]:%s', self.hostname, self.port)
+        logger.info(
+            'MPD server running at %s',
+            network.format_socket_name(server.server_socket))
+
+        return server
 
     def on_start(self):
-        if self.zeroconf_name:
+        if (self.zeroconf_name and not
+                network.is_unix_socket(self.server.server_socket)):
             self.zeroconf_service = zeroconf.Zeroconf(
                 name=self.zeroconf_name,
                 stype='_mpd._tcp',
@@ -75,6 +80,7 @@ class MpdFrontend(pykka.ThreadingActor, CoreListener):
             self.zeroconf_service.unpublish()
 
         process.stop_actors_by_class(session.MpdSession)
+        self.server.stop()
 
     def on_event(self, event, **kwargs):
         if event not in _CORE_EVENTS_TO_IDLE_SUBSYSTEMS:

mopidy/mpd/session.py
@@ -25,18 +25,19 @@ class MpdSession(network.LineProtocol):
             session=self, config=config, core=core, uri_map=uri_map)
 
     def on_start(self):
-        logger.info('New MPD connection from [%s]:%s', self.host, self.port)
+        logger.info('New MPD connection from %s', self.connection)
         self.send_lines(['OK MPD %s' % protocol.VERSION])
 
     def on_line_received(self, line):
-        logger.debug('Request from [%s]:%s: %s', self.host, self.port, line)
+        logger.debug('Request from [%s]: %s', self.connection, line)
 
         response = self.dispatcher.handle_request(line)
         if not response:
             return
 
         logger.debug(
-            'Response to [%s]:%s: %s', self.host, self.port,
+            'Response to [%s]: %s',
+            self.connection,
             formatting.indent(self.terminator.join(response)))
 
         self.send_lines(response)

commit c6d2ee9d9f7b7af856aed4e5f1126e42754d311d

network: log client address not server address. (Fixes #1788)
This, along with some formatting, was broken in #1629.

mopidy/internal/network.py
@@ -24,6 +24,14 @@ def is_unix_socket(sock):
     return False
 
 
+def get_socket_address(host, port):
+    unix_socket_path = path.get_unix_socket_path(host)
+    if unix_socket_path is not None:
+        return (unix_socket_path, None)
+    else:
+        return (host, port)
+
+
 class ShouldRetrySocketCall(Exception):
 
     """Indicate that attempted socket call should be retried"""
@@ -70,12 +78,13 @@ def create_unix_socket():
     return socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
 
-def format_socket_name(sock):
-    """Format the connection string for the given socket"""
-    if is_unix_socket(sock):
-        return '%s' % sock.getsockname()
+def format_address(address):
+    """Format socket address for display."""
+    host, port = address[:2]
+    if port is not None:
+        return '[%s]:%s' % (host, port)
     else:
-        return '[%s]:%s' % sock.getsockname()[:2]
+        return '[%s]' % host
 
 
 def format_hostname(hostname):
@@ -96,6 +105,7 @@ class Server(object):
         self.max_connections = max_connections
         self.timeout = timeout
         self.server_socket = self.create_server_socket(host, port)
+        self.address = get_socket_address(host, port)
 
         self.watcher = self.register_server_socket(self.server_socket.fileno())
 
@@ -166,9 +176,7 @@ class Server(object):
 
     def reject_connection(self, sock, addr):
         # FIXME provide more context in logging?
-        logger.warning(
-            'Rejected connection from %s',
-            format_socket_name(sock))
+        logger.warning('Rejected connection from %s', format_address(addr))
         try:
             sock.close()
         except socket.error:
@@ -356,7 +364,7 @@ class Connection(object):
         return False
 
     def __str__(self):
-        return format_socket_name(self._sock)
+        return format_address((self.host, self.port))
 
 
 class LineProtocol(pykka.ThreadingActor):

mopidy/mpd/actor.py
@@ -61,8 +61,7 @@ class MpdFrontend(pykka.ThreadingActor, CoreListener):
                 encoding.locale_decode(error))
 
         logger.info(
-            'MPD server running at %s',
-            network.format_socket_name(server.server_socket))
+            'MPD server running at %s', network.format_address(server.address))
 
         return server
 

mopidy/mpd/session.py
@@ -29,14 +29,14 @@ class MpdSession(network.LineProtocol):
         self.send_lines(['OK MPD %s' % protocol.VERSION])
 
     def on_line_received(self, line):
-        logger.debug('Request from [%s]: %s', self.connection, line)
+        logger.debug('Request from %s: %s', self.connection, line)
 
         response = self.dispatcher.handle_request(line)
         if not response:
             return
 
         logger.debug(
-            'Response to [%s]: %s',
+            'Response to %s: %s',
             self.connection,
             formatting.indent(self.terminator.join(response)))
 

tests/internal/network/test_connection.py
@@ -557,3 +557,15 @@ class ConnectionTest(unittest.TestCase):
 
         self.assertFalse(network.Connection.timeout_callback(self.mock))
         self.mock.stop.assert_called_once_with(any_unicode)
+
+    def test_str(self):
+        self.mock.host = 'foo'
+        self.mock.port = 999
+
+        self.assertEqual('[foo]:999', network.Connection.__str__(self.mock))
+
+    def test_str_without_port(self):
+        self.mock.host = 'foo'
+        self.mock.port = None
+
+        self.assertEqual('[foo]', network.Connection.__str__(self.mock))

tests/internal/network/test_server.py
@@ -19,6 +19,7 @@ class ServerTest(unittest.TestCase):
     def setUp(self):  # noqa: N802
         self.mock = Mock(spec=network.Server)
 
+    @patch.object(network, 'get_socket_address', new=Mock())
     def test_init_calls_create_server_socket(self):
         network.Server.__init__(
             self.mock, sentinel.host, sentinel.port, sentinel.protocol)
@@ -26,6 +27,16 @@ class ServerTest(unittest.TestCase):
             sentinel.host, sentinel.port)
         self.mock.stop()
 
+    @patch.object(network, 'get_socket_address', new=Mock())
+    def test_init_calls_get_socket_address(self):
+        network.Server.__init__(
+            self.mock, sentinel.host, sentinel.port, sentinel.protocol)
+        self.mock.create_server_socket.return_value = None
+        network.get_socket_address.assert_called_once_with(
+            sentinel.host, sentinel.port)
+        self.mock.stop()
+
+    @patch.object(network, 'get_socket_address', new=Mock())
     def test_init_calls_register_server(self):
         sock = Mock(spec=socket.SocketType)
         sock.fileno.return_value = sentinel.fileno
@@ -36,6 +47,7 @@ class ServerTest(unittest.TestCase):
         self.mock.register_server_socket.assert_called_once_with(
             sentinel.fileno)
 
+    @patch.object(network, 'get_socket_address', new=Mock())
     def test_init_fails_on_fileno_call(self):
         sock = Mock(spec=socket.SocketType)
         sock.fileno.side_effect = socket.error
@@ -51,12 +63,14 @@ class ServerTest(unittest.TestCase):
         self.mock.create_server_socket.return_value = sock
 
         network.Server.__init__(
-            self.mock, sentinel.host, sentinel.port, sentinel.protocol,
+            self.mock, str(sentinel.host), sentinel.port, sentinel.protocol,
             max_connections=sentinel.max_connections, timeout=sentinel.timeout)
         self.assertEqual(sentinel.protocol, self.mock.protocol)
         self.assertEqual(sentinel.max_connections, self.mock.max_connections)
         self.assertEqual(sentinel.timeout, self.mock.timeout)
         self.assertEqual(sock, self.mock.server_socket)
+        self.assertEqual(
+            (str(sentinel.host), sentinel.port), self.mock.address)
 
     def test_create_server_socket_no_port(self):
         with self.assertRaises(exceptions.ValidationError):
@@ -257,7 +271,6 @@ class ServerTest(unittest.TestCase):
             sentinel.protocol, {}, sentinel.sock, sentinel.addr,
             sentinel.timeout)
 
-    @patch.object(network, 'format_socket_name', new=Mock())
     def test_reject_connection(self):
         sock = Mock(spec=socket.SocketType)
 
@@ -265,7 +278,19 @@ class ServerTest(unittest.TestCase):
             self.mock, sock, (sentinel.host, sentinel.port))
         sock.close.assert_called_once_with()
 
-    @patch.object(network, 'format_socket_name', new=Mock())
+    @patch.object(network, 'format_address', new=Mock())
+    @patch.object(network.logger, 'warning', new=Mock())
+    def test_reject_connection_message(self):
+        sock = Mock(spec=socket.SocketType)
+        network.format_address.return_value = sentinel.formatted
+
+        network.Server.reject_connection(
+            self.mock, sock, (sentinel.host, sentinel.port))
+        network.format_address.assert_called_once_with(
+            (sentinel.host, sentinel.port))
+        network.logger.warning.assert_called_once_with(
+            'Rejected connection from %s', sentinel.formatted)
+
     def test_reject_connection_error(self):
         sock = Mock(spec=socket.SocketType)
         sock.close.side_effect = socket.error

tests/internal/network/test_utils.py
@@ -22,23 +22,40 @@ class FormatHostnameTest(unittest.TestCase):
         self.assertEqual(network.format_hostname('0.0.0.0'), '0.0.0.0')
 
 
-class FormatSocketConnectionTest(unittest.TestCase):
+class FormatAddressTest(unittest.TestCase):
 
-    def test_format_socket_name(self):
-        sock = Mock(spec=socket.SocketType)
-        sock.family = socket.AF_INET
-        sock.getsockname.return_value = (sentinel.ip, sentinel.port)
+    def test_format_address_ipv4(self):
+        address = (sentinel.host, sentinel.port)
         self.assertEqual(
-            network.format_socket_name(sock),
-            '[%s]:%s' % (sentinel.ip, sentinel.port))
+            network.format_address(address),
+            '[%s]:%s' % (sentinel.host, sentinel.port))
 
-    def test_format_socket_name_unix(self):
-        sock = Mock(spec=socket.SocketType)
-        sock.family = socket.AF_UNIX
-        sock.getsockname.return_value = sentinel.sockname
+    def test_format_address_ipv6(self):
+        address = (sentinel.host, sentinel.port, sentinel.flow, sentinel.scope)
         self.assertEqual(
-            network.format_socket_name(sock),
-            str(sentinel.sockname))
+            network.format_address(address),
+            '[%s]:%s' % (sentinel.host, sentinel.port))
+
+    def test_format_address_unix(self):
+        address = (sentinel.path, None)
+        self.assertEqual(
+            network.format_address(address),
+            '[%s]' % (sentinel.path))
+
+
+class GetSocketAddress(unittest.TestCase):
+
+    def test_get_socket_address(self):
+        host = str(sentinel.host)
+        port = sentinel.port
+        self.assertEqual(
+            network.get_socket_address(host, port), (host, port))
+
+    def test_get_socket_address_unix(self):
+        host = str(sentinel.host)
+        port = sentinel.port
+        self.assertEqual(
+            network.get_socket_address('unix:' + host, port), (host, None))
 
 
 class TryIPv6SocketTest(unittest.TestCase):

tests/mpd/test_session.py
@@ -1,0 +1,30 @@
+from __future__ import unicode_literals
+
+import logging
+
+from mock import Mock, sentinel
+
+from mopidy.internal import network
+from mopidy.mpd import dispatcher, session
+
+
+def test_on_start_logged(caplog):
+    caplog.set_level(logging.INFO)
+    connection = Mock(spec=network.Connection)
+
+    session.MpdSession(connection).on_start()
+
+    assert 'New MPD connection from %s' % connection in caplog.text
+
+
+def test_on_line_received_logged(caplog):
+    caplog.set_level(logging.DEBUG)
+    connection = Mock(spec=network.Connection)
+    mpd_session = session.MpdSession(connection)
+    mpd_session.dispatcher = Mock(spec=dispatcher.MpdDispatcher)
+    mpd_session.dispatcher.handle_request.return_value = [str(sentinel.resp)]
+
+    mpd_session.on_line_received(sentinel.line)
+
+    assert 'Request from %s: %s' % (connection, sentinel.line) in caplog.text
+    assert 'Response to %s:' % (connection,) in caplog.text