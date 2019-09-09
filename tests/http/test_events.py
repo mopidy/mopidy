c5a62f9cfe53286017cebe59b9abede6fca5fd7f
Nick Steel <nickasteel@gmail.com>
2019-09-09 16:22:47 +0100

    http: fix websocket broadcasts in Tornado 5.x (#1796)
    In Tornado 4.x and earlier, IOLoop.current() would return the global IOLoop
    if there was no IOLoop already running in the calling thread. This was the
    case when calling our websocket broadcast method from the HttpFrontend thread
    and so callbacks were correctly scheduled on the HttpServer thread's IOLoop.
    However, in Tornado 5.0+, the idea of a global IOLoop is gone and calling
    IOLoop.current() will simply create a new IOLoop if there isn't one already
    running in the calling thread.
    This incorrectly resulted in callbacks being scheduled on that new IOLoop
    which is never actually started and so the broadcasts were never sent.
    
    This is related to #1716.
    
    (cherry picked from commit 59a3935b7315ac9296b383f923fefbf4e33e5676)

--- tests/http/test_events.py
+++ tests/http/test_events.py
@@ -11,8 +11,11 @@ from mopidy.http import actor
 @mock.patch('mopidy.http.handlers.WebSocketHandler.broadcast')
 class HttpEventsTest(unittest.TestCase):
 
+    def setUp(self):
+        self.io_loop = mock.Mock()
+
     def test_track_playback_paused_is_broadcasted(self, broadcast):
-        actor.on_event('track_playback_paused', foo='bar')
+        actor.on_event('track_playback_paused', self.io_loop, foo='bar')
 
         self.assertDictEqual(
             json.loads(str(broadcast.call_args[0][0])), {
@@ -21,7 +24,7 @@ class HttpEventsTest(unittest.TestCase):
             })
 
     def test_track_playback_resumed_is_broadcasted(self, broadcast):
-        actor.on_event('track_playback_resumed', foo='bar')
+        actor.on_event('track_playback_resumed', self.io_loop, foo='bar')
 
         self.assertDictEqual(
             json.loads(str(broadcast.call_args[0][0])), {
