e065520ff5238f4fd01cff0c7034b29afcd8e953
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-10-11 00:28:49 +0200

    mtimes: Reorder functions

--- mopidy/internal/mtimes.py
+++ mopidy/internal/mtimes.py
@@ -7,6 +7,49 @@ from mopidy.compat import queue
 from mopidy.internal import encoding
 
 
+def find_mtimes(root, follow=False):
+    results, errors = _find(root, relative=False, follow=follow)
+    # return the mtimes as integer milliseconds
+    mtimes = {f: int(st.st_mtime * 1000) for f, st in results.items()}
+    return mtimes, errors
+
+
+def _find(root, thread_count=10, relative=False, follow=False):
+    """Threaded find implementation that provides stat results for files.
+
+    Tries to protect against sym/hardlink loops by keeping an eye on parent
+    (st_dev, st_ino) pairs.
+
+    :param str root: root directory to search from, may not be a file
+    :param int thread_count: number of workers to use, mainly useful to
+        mitigate network lag when scanning on NFS etc.
+    :param bool relative: if results should be relative to root or absolute
+    :param bool follow: if symlinks should be followed
+    """
+    threads = []
+    results = {}
+    errors = {}
+    done = threading.Event()
+    work = queue.Queue()
+    work.put((os.path.abspath(root), []))
+
+    if not relative:
+        root = None
+
+    args = (root, follow, done, work, results, errors)
+    for i in range(thread_count):
+        t = threading.Thread(target=_find_worker, args=args)
+        t.daemon = True
+        t.start()
+        threads.append(t)
+
+    work.join()
+    done.set()
+    for t in threads:
+        t.join()
+    return results, errors
+
+
 def _find_worker(relative, follow, done, work, results, errors):
     """Worker thread for collecting stat() results.
 
@@ -54,46 +97,3 @@ def _find_worker(relative, follow, done, work, results, errors):
                 encoding.locale_decode(e.strerror), e.errno)
         finally:
             work.task_done()
-
-
-def _find(root, thread_count=10, relative=False, follow=False):
-    """Threaded find implementation that provides stat results for files.
-
-    Tries to protect against sym/hardlink loops by keeping an eye on parent
-    (st_dev, st_ino) pairs.
-
-    :param str root: root directory to search from, may not be a file
-    :param int thread_count: number of workers to use, mainly useful to
-        mitigate network lag when scanning on NFS etc.
-    :param bool relative: if results should be relative to root or absolute
-    :param bool follow: if symlinks should be followed
-    """
-    threads = []
-    results = {}
-    errors = {}
-    done = threading.Event()
-    work = queue.Queue()
-    work.put((os.path.abspath(root), []))
-
-    if not relative:
-        root = None
-
-    args = (root, follow, done, work, results, errors)
-    for i in range(thread_count):
-        t = threading.Thread(target=_find_worker, args=args)
-        t.daemon = True
-        t.start()
-        threads.append(t)
-
-    work.join()
-    done.set()
-    for t in threads:
-        t.join()
-    return results, errors
-
-
-def find_mtimes(root, follow=False):
-    results, errors = _find(root, relative=False, follow=follow)
-    # return the mtimes as integer milliseconds
-    mtimes = {f: int(st.st_mtime * 1000) for f, st in results.items()}
-    return mtimes, errors
