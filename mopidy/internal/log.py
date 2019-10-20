b868a8e991ae5c6d26a1ee4d7e12de473f784e27
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-10-20 23:17:43 +0200

    py3: Fix iteritems() in mopidy.internal.log

--- mopidy/internal/log.py
+++ mopidy/internal/log.py
@@ -5,6 +5,8 @@ import logging.config
 import logging.handlers
 import platform
 
+from mopidy import compat
+
 
 LOG_LEVELS = {
     -1: dict(root=logging.ERROR, mopidy=logging.WARNING),
@@ -180,7 +182,7 @@ class ColorizingStreamHandler(logging.StreamHandler):
         message = logging.StreamHandler.format(self, record)
         if not self.is_tty or self.is_windows:
             return message
-        for name, color in self.logger_map.iteritems():
+        for name, color in compat.iteritems(self.logger_map):
             if record.name.startswith(name):
                 return self.colorize(message, fg=color)
         if record.levelno in self.level_map:
