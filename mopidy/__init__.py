cc2fa4ba8cf7e099ddda920ef166082065aa58fd
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-11-14 15:58:11 +0100

    setup: Specify version number in setup.cfg

--- mopidy/__init__.py
+++ mopidy/__init__.py
@@ -2,14 +2,14 @@ import platform
 import sys
 import warnings
 
+import pkg_resources
+
 if not sys.version_info >= (3, 7):
     sys.exit(
         f"ERROR: Mopidy requires Python >= 3.7, "
         f"but found {platform.python_version()}."
     )
 
-
 warnings.filterwarnings("ignore", "could not open display")
 
-
-__version__ = "3.0.0a4"
+__version__ = pkg_resources.get_distribution("Mopidy").version
