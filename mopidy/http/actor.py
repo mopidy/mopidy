095b2d12c0ea9844f8e94181896708c32b70710b
Vivien Henry <vivien.henry@outlook.fr>
2019-12-01 12:09:16 +0100

    formatting with black

--- mopidy/http/actor.py
+++ mopidy/http/actor.py
@@ -199,5 +199,7 @@ class HttpServer(threading.Thread):
         else:
             cookie_secret = file_path.read_text().strip()
             if not cookie_secret:
-                logging.error(f"HTTP server could not find cookie secret in {file_path}")
+                logging.error(
+                    f"HTTP server could not find cookie secret in {file_path}"
+                )
         return cookie_secret
