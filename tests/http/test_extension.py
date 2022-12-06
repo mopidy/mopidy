mopidy/mopidy

commit 1df50d170553538b6f881279ba7ae2ddfffcaef5

Improve handling of "allowed_origins" HTTP config (#2054)
This moves all of the processing for the "allowed_origins" HTTP setting
into the config system, allowing it to be more easily reused by other
extensions. It takes advantage of some new functionality in the List and
String config types.

mopidy/http/__init__.py
@@ -23,7 +23,10 @@ class Extension(ext.Extension):
         schema["port"] = config_lib.Port()
         schema["static_dir"] = config_lib.Deprecated()
         schema["zeroconf"] = config_lib.String(optional=True)
-        schema["allowed_origins"] = config_lib.List(optional=True)
+        schema["allowed_origins"] = config_lib.List(
+            unique=True,
+            subtype=config_lib.String(transformer=lambda x: x.lower()),
+        )
         schema["csrf_protection"] = config_lib.Boolean(optional=True)
         schema["default_app"] = config_lib.String(optional=True)
         return schema

mopidy/http/handlers.py
@@ -28,16 +28,14 @@ def make_mopidy_app_factory(apps, statics):
             logger.warning(
                 "HTTP Cross-Site Request Forgery protection is disabled"
             )
-        allowed_origins = {
-            x.lower() for x in config["http"]["allowed_origins"] if x
-        }
+
         return [
             (
                 r"/ws/?",
                 WebSocketHandler,
                 {
                     "core": core,
-                    "allowed_origins": allowed_origins,
+                    "allowed_origins": config["http"]["allowed_origins"],
                     "csrf_protection": config["http"]["csrf_protection"],
                 },
             ),
@@ -46,7 +44,7 @@ def make_mopidy_app_factory(apps, statics):
                 JsonRpcHandler,
                 {
                     "core": core,
-                    "allowed_origins": allowed_origins,
+                    "allowed_origins": config["http"]["allowed_origins"],
                     "csrf_protection": config["http"]["csrf_protection"],
                 },
             ),
@@ -177,13 +175,13 @@ def check_origin(origin, request_headers, allowed_origins):
     if origin is None:
         logger.warning("HTTP request denied for missing Origin header")
         return False
-    allowed_origins.add(request_headers.get("Host"))
+    host_header = request_headers.get("Host")
     parsed_origin = urllib.parse.urlparse(origin).netloc.lower()
     # Some frameworks (e.g. Apache Cordova) use local files. Requests from
     # these files don't really have a sensible Origin so the browser sets the
     # header to something like 'file://' or 'null'. This results here in an
     # empty parsed_origin which we choose to allow.
-    if parsed_origin and parsed_origin not in allowed_origins:
+    if parsed_origin and parsed_origin not in allowed_origins | {host_header}:
         logger.warning('HTTP request denied for Origin "%s"', origin)
         return False
     return True

tests/http/test_handlers.py
@@ -51,7 +51,7 @@ class WebSocketHandlerTest(tornado.testing.AsyncHTTPTestCase):
                     handlers.WebSocketHandler,
                     {
                         "core": self.core,
-                        "allowed_origins": set(),
+                        "allowed_origins": frozenset(),
                         "csrf_protection": True,
                     },
                 )

tests/http/test_server.py
@@ -18,7 +18,7 @@ class HttpServerTest(tornado.testing.AsyncHTTPTestCase):
                 "hostname": "127.0.0.1",
                 "port": 6680,
                 "zeroconf": "",
-                "allowed_origins": [],
+                "allowed_origins": frozenset(),
                 "csrf_protection": True,
                 "default_app": "mopidy",
             }

commit 2acdc1b838c3e0780b0801cfdebe1bc36a8e374e

http: Fixed optional allowed_origins config setting.
This was broken in #2054

mopidy/http/__init__.py
@@ -24,6 +24,7 @@ class Extension(ext.Extension):
         schema["static_dir"] = config_lib.Deprecated()
         schema["zeroconf"] = config_lib.String(optional=True)
         schema["allowed_origins"] = config_lib.List(
+            optional=True,
             unique=True,
             subtype=config_lib.String(transformer=lambda x: x.lower()),
         )

tests/http/test_extension.py
@@ -1,0 +1,35 @@
+from mopidy import config as config_lib
+from mopidy.http import Extension
+
+
+def test_get_default_config():
+    ext = Extension()
+
+    config = ext.get_default_config()
+
+    assert "[http]" in config
+    assert "enabled = true" in config
+
+
+def test_get_config_schema():
+    ext = Extension()
+
+    schema = ext.get_config_schema()
+
+    assert "enabled" in schema
+    assert "hostname" in schema
+    assert "port" in schema
+    assert "zeroconf" in schema
+    assert "allowed_origins" in schema
+    assert "csrf_protection" in schema
+    assert "default_app" in schema
+
+
+def test_default_config_is_valid():
+    ext = Extension()
+
+    config = ext.get_default_config()
+    schema = ext.get_config_schema()
+    _, errors = config_lib.load([], [schema], [config], [])
+
+    assert errors.get("http") is None