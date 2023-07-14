d7d159a09a6988d4f72a96860d8d49ecf3d94609
jan iversen <jancasacondor@gmail.com>
2023-07-14 16:49:52 +0200

    tests:Allow tests to run on MacOS.

--- tests/test_ext.py
+++ tests/test_ext.py
@@ -251,7 +251,7 @@ class TestValidateExtensionData:
         with mock.patch.object(ext.path, "get_or_create_dir"):
             cache_dir = extension.get_cache_dir(config)
 
-        expected = pathlib.Path(core_cache_dir) / extension.ext_name
+        expected = pathlib.Path(core_cache_dir).resolve() / extension.ext_name
         assert cache_dir == expected
 
     def test_get_config_dir(self, ext_data):
@@ -262,7 +262,7 @@ class TestValidateExtensionData:
         with mock.patch.object(ext.path, "get_or_create_dir"):
             config_dir = extension.get_config_dir(config)
 
-        expected = pathlib.Path(core_config_dir) / extension.ext_name
+        expected = pathlib.Path(core_config_dir).resolve() / extension.ext_name
         assert config_dir == expected
 
     def test_get_data_dir(self, ext_data):
@@ -273,5 +273,5 @@ class TestValidateExtensionData:
         with mock.patch.object(ext.path, "get_or_create_dir"):
             data_dir = extension.get_data_dir(config)
 
-        expected = pathlib.Path(core_data_dir) / extension.ext_name
+        expected = pathlib.Path(core_data_dir).resolve() / extension.ext_name
         assert data_dir == expected
