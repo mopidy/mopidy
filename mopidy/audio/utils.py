c8d849aa6e8c27707f62a331dd7f90ebadb74ebb
Stein Magnus Jodal <stein.magnus@jodal.no>
2023-08-07 23:49:43 +0200

    Fix F811 lint

--- mopidy/audio/utils.py
+++ mopidy/audio/utils.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Callable, Iterable, cast
+from collections.abc import Iterable
+from typing import TYPE_CHECKING, Any, Callable, cast
 
 from mopidy import httpclient
 from mopidy.internal.gi import Gst
