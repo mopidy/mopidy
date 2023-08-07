22f0848dcc46986f48782321898e15919a5949c7
jan iversen <jancasacondor@gmail.com>
2023-08-07 18:27:29 +0200

    Clean __init__ and make external symbols.

--- mopidy/audio/__init__.py
+++ mopidy/audio/__init__.py
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 from .actor import Audio, AudioProxy
+=======
+# flake8: noqa
+from .actor import Audio
+>>>>>>> d17d241c (Clean __init__ and make external symbols.)
 from .constants import PlaybackState
 from .listener import AudioListener
 from .utils import supported_uri_schemes
