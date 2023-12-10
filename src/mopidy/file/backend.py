e76169509be24d39ec6dff5f8e2d0c0e4c2df37d
PRO X <bredalisgautreaux@gmail.com>
2023-12-10 10:57:11 -0400

    backend.py (#1)
    Inserted comments

--- src/mopidy/file/backend.py
+++ src/mopidy/file/backend.py
@@ -6,14 +6,23 @@ import pykka
 from mopidy import backend
 from mopidy.file import library
 
+# Event logging setup
 logger = logging.getLogger(__name__)
 
 
 class FileBackend(pykka.ThreadingActor, backend.Backend):
+    # URI schemes supported by this backend
     uri_schemes: ClassVar[list[str]] = ["file"]
 
     def __init__(self, config, audio):
+        # Backend initialization
         super().__init__()
+
+        # File library provider
         self.library = library.FileLibraryProvider(backend=self, config=config)
+
+        # Audio playback provider
         self.playback = backend.PlaybackProvider(audio=audio, backend=self)
+
+        # Playlist initialization is not yet implemented
         self.playlists = None
