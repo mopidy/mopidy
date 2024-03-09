993520a657736ead225e23bcebf5e060974e2322
Stein Magnus Jodal <stein.magnus.jodal@oda.com>
2024-03-09 18:54:31 +0100

    types: Reorder

--- src/mopidy/types.py
+++ src/mopidy/types.py
@@ -6,6 +6,15 @@ from typing import TYPE_CHECKING, Literal, NewType, TypeVar
 if TYPE_CHECKING:
     from typing import TypeAlias
 
+# Integer types
+Percentage = NewType("Percentage", int)
+DurationMs = NewType("DurationMs", int)
+
+# URI types
+Uri = NewType("Uri", str)
+UriScheme = NewType("UriScheme", str)
+
+# Query types
 F = TypeVar("F")
 QueryValue: TypeAlias = str | int
 Query: TypeAlias = dict[F, Iterable[QueryValue]]
@@ -33,7 +42,7 @@ DistinctField: TypeAlias = Literal[
 SearchField: TypeAlias = DistinctField | Literal["any"]
 SearchQuery: TypeAlias = dict[SearchField, Iterable[QueryValue]]
 
-# Types for tracklist filtering
+# Tracklist types
 TracklistField: TypeAlias = Literal[
     "tlid",
     "uri",
@@ -45,11 +54,3 @@ TracklistField: TypeAlias = Literal[
 
 # Superset of all fields that can be used in a query
 QueryField: TypeAlias = DistinctField | SearchField | TracklistField
-
-# URI types
-Uri = NewType("Uri", str)
-UriScheme = NewType("UriScheme", str)
-
-# Integer types
-Percentage = NewType("Percentage", int)
-DurationMs = NewType("DurationMs", int)
