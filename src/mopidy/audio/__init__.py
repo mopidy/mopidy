ab7027e1b16d3cf1a20b3408b43f542ac16124e2
Aarni Koskela <akx@iki.fi>
2025-01-14 13:35:58 +0200

    Refactor `audio` package

--- src/mopidy/audio/__init__.py
+++ src/mopidy/audio/__init__.py
@@ -1,13 +1,19 @@
+from mopidy.audio.gst import scan, tags
+from mopidy.audio.gst.actor import Audio
+from mopidy.audio.gst.utils import supported_uri_schemes
+from mopidy.audio.listener import AudioListener
+from mopidy.audio.utils import _make_audio_proxy
 from mopidy.types import PlaybackState
 
-from .actor import Audio, AudioProxy
-from .listener import AudioListener
-from .utils import supported_uri_schemes
+AudioProxy = _make_audio_proxy(Audio)
+
 
 __all__ = [
     "Audio",
     "AudioListener",
     "AudioProxy",
     "PlaybackState",  # Re-exported for backwards compatibility.
+    "scan",
     "supported_uri_schemes",
+    "tags",
 ]
