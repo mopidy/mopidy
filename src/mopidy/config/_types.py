fc76acf3956495114bbec076b9e3051e2bdf8132
Stein Magnus Jodal <stein.magnus@jodal.no>
2026-01-03 19:26:45 +0100

    feat: Make Config return the right type for each config section
    The old implementation of `Config` gave us the right type for Mopidy's
    own config sections, but did not work well for extension's config
    sections.

--- src/mopidy/config/_types.py
+++ src/mopidy/config/_types.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 import pathlib
-from typing import Any, Literal, TypedDict
+from collections.abc import Iterator, Mapping
+from typing import Any, Literal, TypedDict, overload, override
 
 LogColorName = Literal[
     "black",
@@ -26,13 +27,78 @@ LogLevelName = Literal[
 type ConfigDict = dict[str, dict[str, Any]]
 
 
-class Config(TypedDict):
-    core: CoreConfig
-    logging: LoggingConfig
-    loglevels: dict[LogLevelName, int]
-    logcolors: dict[LogLevelName, LogColorName]
-    audio: AudioConfig
-    proxy: ProxyConfig
+class Config(Mapping):
+    def __init__(self, data: dict[str, dict[str, Any]]) -> None:
+        self._data = data
+
+    @overload
+    def __getitem__(self, key: Literal["core"]) -> CoreConfig: ...  # pyright: ignore[reportOverlappingOverload]
+    @overload
+    def __getitem__(self, key: Literal["logging"]) -> LoggingConfig: ...
+    @overload
+    def __getitem__(self, key: Literal["loglevels"]) -> dict[LogLevelName, int]: ...
+    @overload
+    def __getitem__(
+        self, key: Literal["logcolors"]
+    ) -> dict[LogLevelName, LogColorName]: ...
+    @overload
+    def __getitem__(self, key: Literal["audio"]) -> AudioConfig: ...
+    @overload
+    def __getitem__(self, key: Literal["proxy"]) -> ProxyConfig: ...
+    @overload
+    def __getitem__(self, key: ...) -> ConfigSection: ...
+
+    @override
+    def __getitem__(
+        self, key: str
+    ) -> (
+        ConfigSection
+        | CoreConfig
+        | LoggingConfig
+        | dict[LogLevelName, int]
+        | dict[LogLevelName, LogColorName]
+        | AudioConfig
+        | ProxyConfig
+    ):
+        return ConfigSection(key, self._data.__getitem__(key))
+
+    def __setitem__(self, key: str, value: dict[str, Any]) -> None:
+        self._data[key] = value
+
+    @override
+    def __iter__(self) -> Iterator[str]:
+        return self._data.__iter__()
+
+    @override
+    def __len__(self) -> int:
+        return self._data.__len__()
+
+    def __repr__(self) -> str:
+        return f"Config({self._data!r})"
+
+
+class ConfigSection(Mapping):
+    def __init__(self, name: str, data: dict[str, Any]) -> None:
+        self._name = name
+        self._data = data
+
+    @override
+    def __getitem__(self, key: str) -> Any:
+        return self._data.__getitem__(key)
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self._data[key] = value
+
+    @override
+    def __iter__(self) -> Iterator[str]:
+        return self._data.__iter__()
+
+    @override
+    def __len__(self) -> int:
+        return self._data.__len__()
+
+    def __repr__(self) -> str:
+        return f"ConfigSection({self._name!r}, {self._data!r})"
 
 
 class CoreConfig(TypedDict):
