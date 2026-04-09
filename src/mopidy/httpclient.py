b5dc2af86d579a8563b9d7566fd31de2b533f90f
Stein Magnus Jodal <stein.magnus@jodal.no>
2026-04-09 11:41:28 +0200

    docs: Migrate httpclient and paths docstrings to Markdown/Google style

--- src/mopidy/httpclient.py
+++ src/mopidy/httpclient.py
@@ -14,13 +14,11 @@ if TYPE_CHECKING:
 def format_proxy(proxy_config: ProxyConfig, auth: bool = True) -> str | None:
     """Convert a Mopidy proxy config to the commonly used proxy string format.
 
-    Outputs ``scheme://host:port``, ``scheme://user:pass@host:port`` or
-    :class:`None` depending on the proxy config provided.
+    Outputs `scheme://host:port`, `scheme://user:pass@host:port` or `None`
+    depending on the proxy config provided.
 
-    You can also opt out of getting the basic auth by setting ``auth`` to
-    :class:`False`.
-
-    .. versionadded:: 1.1
+    You can also opt out of getting the basic auth by setting `auth` to
+    `False`.
     """
     if not proxy_config.get("hostname"):
         return None
@@ -42,10 +40,8 @@ def format_proxy(proxy_config: ProxyConfig, auth: bool = True) -> str | None:
 def format_user_agent(name: str | None = None) -> str:
     """Construct a User-Agent suitable for use in client code.
 
-    This will identify use by the provided ``name`` (which should be on the
-    format ``dist_name/version``), Mopidy version and Python version.
-
-    .. versionadded:: 1.1
+    This will identify use by the provided `name` (which should be on the
+    format `dist_name/version`), Mopidy version and Python version.
     """
     parts = [
         f"Mopidy/{mopidy.__version__}",
