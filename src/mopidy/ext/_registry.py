894f1da230b0ed4fd5d99a34de99c6a9cb27c310
Stein Magnus Jodal <stein.magnus@jodal.no>
2026-04-09 11:42:00 +0200

    docs: Migrate ext API docstrings to Markdown/Google style

--- src/mopidy/ext/_registry.py
+++ src/mopidy/ext/_registry.py
@@ -14,20 +14,19 @@ type RegistryEntry = type[Any] | dict[str, Any]
 class Registry(Mapping):
     """Registry of components provided by Mopidy extensions.
 
-    Passed to the :meth:`~Extension.setup` method of all extensions. The
-    registry can be used like a dict of string keys and lists.
+    Passed to the [setup][mopidy.ext.Extension.setup] method of all
+    extensions. The registry can be used like a dict of string keys and lists.
 
     Some keys have a special meaning, including, but not limited to:
 
-    - ``backend`` is used for Mopidy backend classes.
-    - ``frontend`` is used for Mopidy frontend classes.
+    - `backend` is used for Mopidy backend classes.
+    - `frontend` is used for Mopidy frontend classes.
 
-    Extensions can use the registry for allow other to extend the extension
-    itself. For example the ``Mopidy-Local`` historically used the
-    ``local:library`` key to allow other extensions to register library
-    providers for ``Mopidy-Local`` to use. Extensions should namespace
-    custom keys with the extension's :attr:`~Extension.ext_name`,
-    e.g. ``local:foo`` or ``http:bar``.
+    Extensions can use the registry to allow others to extend the extension
+    itself. For example, `Mopidy-Local` historically used the `local:library`
+    key to allow other extensions to register library providers for it to use.
+    Extensions should namespace custom keys with the extension's
+    [ext_name][mopidy.ext.Extension.ext_name], e.g. `local:foo` or `http:bar`.
     """
 
     def __init__(self) -> None:
