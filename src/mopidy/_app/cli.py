ebf438d2e2e9a24c62c7796d48cf11821b770569
Stein Magnus Jodal <stein.magnus@jodal.no>
2026-05-03 13:26:33 +0200

    fix: Use Parameter in a way that works before cyclopts 4.3

--- src/mopidy/_app/cli.py
+++ src/mopidy/_app/cli.py
@@ -54,33 +54,12 @@ def config_paths_display(value: list[Path]) -> str:
     return ", ".join(str(path) for path in value)
 
 
-@Parameter(
-    name="--config",
-    help=(
-        "Config files to use. "
-        "Repeat parameter or separate values with colon to use multiple files. "
-        "Later files have higher precedence."
-    ),
-    show_default=config_paths_display,
-    negative="",
-    n_tokens=1,
-)
 def config_paths_converter(_: type, tokens: Sequence[Token]) -> list[Path]:
     return [
         Path(path).expanduser() for token in tokens for path in token.value.split(":")
     ]
 
 
-@Parameter(
-    name=("--option", "-o"),
-    help=(
-        "Override config values. "
-        "Repeat parameter to override multiple values. "
-        "Format: SECTION/KEY=VALUE."
-    ),
-    negative="",
-    n_tokens=1,
-)
 def config_overrides_converter(
     _: type, tokens: Sequence[Token]
 ) -> list[dict[str, dict[str, str]]]:
@@ -110,11 +89,36 @@ def launcher(
     ],
     config_paths: Annotated[
         list[Path],
-        Parameter(converter=config_paths_converter),
+        Parameter(
+            # NOTE: When upgrading to cyclopts >= 4.3, this can be moved to a
+            # @Parameter decorator on the config_paths_converter function, with
+            # Parameter(n_tokens=1).
+            name="--config",
+            help=(
+                "Config files to use. "
+                "Repeat parameter or separate values with colon to use multiple files. "
+                "Later files have higher precedence."
+            ),
+            show_default=config_paths_display,
+            negative="",
+            converter=config_paths_converter,
+        ),
     ] = config_paths_default(),  # noqa: B008
     config_overrides: Annotated[
         list[ConfigOverrides] | None,
-        Parameter(converter=config_overrides_converter),
+        Parameter(
+            # NOTE: When upgrading to cyclopts >= 4.3, this can be moved to a
+            # @Parameter decorator on the config_overrides_converter function,
+            # with Parameter(n_tokens=1).
+            name=("--option", "-o"),
+            help=(
+                "Override config values. "
+                "Repeat parameter to override multiple values. "
+                "Format: SECTION/KEY=VALUE."
+            ),
+            negative="",
+            converter=config_overrides_converter,
+        ),
     ] = None,
     quiet: Annotated[
         bool,
