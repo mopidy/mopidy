a0698c99225403229f8e3492bfcfa4f97bc0edbe
Stein Magnus Jodal <stein.magnus@jodal.no>
2026-09-19 12:51:31 +0200

    test: Port backend, config and lib tests from unittest to pytest

--- tests/backend/test_listener.py
+++ tests/backend/test_listener.py
@@ -1,19 +1,22 @@
-import unittest
 from unittest import mock
+
+import pytest
 
 from mopidy import backend
 
 
-class BackendListenerTest(unittest.TestCase):
-    def setUp(self):
-        self.listener = backend.BackendListener()
+@pytest.fixture
+def listener():
+    return backend.BackendListener()
 
-    def test_on_event_forwards_to_specific_handler(self):
-        self.listener.playlists_loaded = mock.Mock()
 
-        self.listener.on_event("playlists_loaded")
+def test_on_event_forwards_to_specific_handler(listener):
+    listener.playlists_loaded = mock.Mock()
 
-        self.listener.playlists_loaded.assert_called_with()
+    listener.on_event("playlists_loaded")
 
-    def test_listener_has_default_impl_for_playlists_loaded(self):
-        self.listener.playlists_loaded()
+    listener.playlists_loaded.assert_called_with()
+
+
+def test_listener_has_default_impl_for_playlists_loaded(listener):
+    listener.playlists_loaded()
